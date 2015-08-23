6bb684ed53ba67b2b5d7e22641c90b93b6750013
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-08-23 15:48:52 +0800

    add click feature

--- library/src/main/java/com/example/peacepassion/library/WeixinTabLayout.java
+++ library/src/main/java/com/example/peacepassion/library/WeixinTabLayout.java
@@ -1,7 +1,6 @@
 package com.example.peacepassion.library;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
@@ -22,6 +21,7 @@ import java.util.List;
  */
 public class WeixinTabLayout extends ViewGroup implements OnPageChangeListener {
 
+    private ViewPager viewPager;
     private List<UnitViewHolder> viewHolders = new ArrayList<>();
     private int selectedPage = 0;
 
@@ -37,7 +37,8 @@ public class WeixinTabLayout extends ViewGroup implements OnPageChangeListener {
         super(context, attrs, defStyleAttr);
     }
 
-    public void setUpViewPager(ViewPager viewPager, PagerAdapter adapter, List<DataHolder> dataHolders) {
+    public void setUpViewPager(final ViewPager viewPager, final PagerAdapter adapter, List<DataHolder> dataHolders) {
+        this.viewPager = viewPager;
         viewPager.addOnPageChangeListener(this);
         if (adapter.getCount() == 0) {
             throw new IllegalStateException("adapter should has at least one item");
@@ -45,8 +46,17 @@ public class WeixinTabLayout extends ViewGroup implements OnPageChangeListener {
         if (adapter.getCount() != dataHolders.size()) {
             throw new IllegalStateException("adapter should has the same length as unit view holders");
         }
-        for (DataHolder dataHolder : dataHolders) {
+        for (int i = 0; i < dataHolders.size(); ++i) {
             RelativeLayout v = (RelativeLayout) LayoutInflater.from(getContext()).inflate(R.layout.unit, this, false);
+            final int j = i;
+            v.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selectedPage = j;
+                    update();
+                }
+            });
+            DataHolder dataHolder = dataHolders.get(i);
             TextView tv = (TextView) v.findViewById(R.id.text);
             tv.setText(dataHolder.title);
             ImageView iv = (ImageView) v.findViewById(R.id.image_bg);
@@ -58,7 +68,18 @@ public class WeixinTabLayout extends ViewGroup implements OnPageChangeListener {
             addView(v);
         }
         selectedPage = 0;
-        viewHolders.get(selectedPage).front.setAlpha(255);
+        update();
+    }
+
+    private void update() {
+        viewPager.setCurrentItem(selectedPage, false);
+        for (int i = 0; i < viewHolders.size(); i++) {
+            if (selectedPage == i) {
+                viewHolders.get(selectedPage).front.setAlpha(255);
+            } else {
+                viewHolders.get(i).front.setAlpha(0);
+            }
+        }
     }
 
     @Override
@@ -72,7 +93,7 @@ public class WeixinTabLayout extends ViewGroup implements OnPageChangeListener {
         }
         else {
             int cw = wSize / count;
-            measureChildren(MeasureSpec.makeMeasureSpec(cw, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(hSize, MeasureSpec.AT_MOST));
+            measureChildren(MeasureSpec.makeMeasureSpec(cw, MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(hSize, MeasureSpec.AT_MOST));
             hSize = getChildAt(0).getMeasuredHeight() + getPaddingBottom() + getPaddingTop();
         }
         setMeasuredDimension(wSize, hSize);
@@ -82,12 +103,12 @@ public class WeixinTabLayout extends ViewGroup implements OnPageChangeListener {
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         int count = getChildCount();
         if (count != 0) {
-            int l = 0, t = getPaddingTop(), r = 0, b = t + getMeasuredHeight(), cw = getMeasuredWidth() / count;
+            int t = getPaddingTop(), r = 0, cw = getMeasuredWidth() / count;
             for (int i = 0; i < count; ++i) {
                 View child = getChildAt(i);
+                int rl = r + (cw - child.getMeasuredWidth()) / 2;
+                child.layout(rl, t, rl + child.getMeasuredWidth(), t + child.getMeasuredHeight());
                 r += cw;
-                child.layout(l, t, r, b);
-                l = r;
             }
         }
     }
