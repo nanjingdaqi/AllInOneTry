eaa6842d61a6aef62ea9b380f9606883b6cb0427
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-08-23 09:48:01 +0800

    try basic toolbar usage

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,12 +1,17 @@
 package org.peace.allinone.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 
@@ -15,12 +20,39 @@ public class MainActivity extends AppCompatActivity {
     @InjectView(R.id.start_btn)
     Button mStartBtn;
 
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.inject(this);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Customized Title");
+        toolbar.setSubtitle("Sub Title");
+        toolbar.setLogo(R.drawable.ic_launcher);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        toolbar.setNavigationContentDescription("Custom up description");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        AppLogger.d("menu item is clicked: " + item.getTitle());
+        return true;
     }
 
     @OnClick({R.id.start_btn})
