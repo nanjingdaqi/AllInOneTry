277802a28dfb1d0cb07a9b53ed2053a0e4de37bf
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-09-04 19:49:29 +0800

    try basic imageSpan

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,9 +1,13 @@
 package org.peace.allinone.ui;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -15,12 +19,24 @@ public class MainActivity extends AppCompatActivity {
     @InjectView(R.id.start_btn)
     Button mStartBtn;
 
+    @InjectView(R.id.text)
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.inject(this);
+
+        String content = "( % 额配送会员 )";
+        SpannableString ss = new SpannableString(content);
+        Drawable vip = getResources().getDrawable(R.drawable.order_confirm_vip_tag);
+        vip.setBounds(0, 0, vip.getIntrinsicWidth(), vip.getIntrinsicHeight());
+        ImageSpan span = new ImageSpan(vip, ImageSpan.ALIGN_BASELINE);
+        int index = content.indexOf('%');
+        ss.setSpan(span, index, index + 1, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(ss);
     }
 
     @OnClick({R.id.start_btn})
