2781288f0277576a2775557d9a3e12b59b1f0f5c
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-09-10 14:55:41 +0800

    finish basic method using pop up window

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,11 +2,23 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.MeasureSpec;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewTreeObserver.OnPreDrawListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.PopupWindow;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
+import me.ele.commons.DimenUtil;
 import org.peace.allinone.R;
 
 
@@ -14,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
 
     @InjectView(R.id.start_btn)
     Button mStartBtn;
+    @InjectView(R.id.edit_text)
+    EditText editText;
+    @InjectView(R.id.pop_up_container)
+    FrameLayout popContainer;
+    @InjectView(R.id.container)
+    ViewGroup container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +45,39 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v) {
         int id = v.getId();
         if (id == R.id.start_btn) {
-
+            editText.requestFocus();
+            editText.setSelection(editText.getText().length());
+            showPopup();
         }
     }
+
+    private void showPopup() {
+        final int[] xy = getDetailCursorPosition();
+        final int x = xy[0], y = xy[1];
+        AppLogger.d("x: " + xy[0] + ", y: " + xy[1]);
+        int[] edLoc = new int[2];
+        editText.getLocationOnScreen(edLoc);
+        AppLogger.d("ed x: " + edLoc[0] + ", ed y: " + edLoc[1]);
+        View v = LayoutInflater.from(this).inflate(R.layout.triangle_pop, null);
+        PopupWindow popupWindow = new PopupWindow(v, DimenUtil.dip2px(this, 160), LayoutParams.WRAP_CONTENT, false);
+//        popupWindow.setWidth(DimenUtil.dip2px(this, 160));
+        popupWindow.setHeight(DimenUtil.dip2px(this, 60));
+        int xoff = x - DimenUtil.dip2px(this, 160) / 2;
+        AppLogger.d("xoff: " + xoff);
+        popupWindow.showAtLocation(container, Gravity.LEFT | Gravity.TOP, edLoc[0] + xoff,
+                edLoc[1] - DimenUtil.dip2px(this, 60) - DimenUtil.dip2px(this, 8));
+        AppLogger.d("pw: " + popupWindow.getWidth() + ", ph: " + popupWindow.getHeight());
+    }
+
+    private int[] getDetailCursorPosition() {
+        int pos = editText.getSelectionStart();
+        Layout layout = editText.getLayout();
+        int line = layout.getLineForOffset(pos);
+        int baseline = layout.getLineBaseline(line);
+        int ascent = layout.getLineAscent(line);
+        float x = layout.getPrimaryHorizontal(pos) + editText.getPaddingLeft();
+        float y = baseline + ascent;
+        return new int[] {(int) x, (int) y};
+    }
+
 }
---
+++ app/src/main/java/org/peace/allinone/ui/TriangleRectPopTextView.java
@@ -0,0 +1,80 @@
+package org.peace.allinone.ui;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.TextView;
+import me.ele.commons.DimenUtil;
+import org.peace.allinone.R;
+
+public class TriangleRectPopTextView extends TextView {
+
+    private Paint paint;
+
+    private int triangleHeight;
+    private int radius;
+    private int backgroundColor;
+
+    private int[] systemAttrs = {android.R.attr.background};
+
+    public TriangleRectPopTextView(Context context) {
+        this(context, null);
+    }
+
+    public TriangleRectPopTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TriangleRectPopTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, systemAttrs);
+        backgroundColor = ta.getColor(0, getResources().getColor(R.color.blue));
+        ta.recycle();
+        ta = context.obtainStyledAttributes(attrs, R.styleable.TriangleRectPopTextView);
+        triangleHeight = ta.getDimensionPixelSize(R.styleable.TriangleRectPopTextView_triangle_height, DimenUtil.dip2px(context, 8));
+        radius = ta.getDimensionPixelSize(R.styleable.TriangleRectPopTextView_rect_radius, DimenUtil.dip2px(context, 4));
+        ta.recycle();
+
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(backgroundColor);
+        paint.setStyle(Style.FILL);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int h = getMeasuredHeight();
+        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), h + triangleHeight);
+        setPadding(getPaddingLeft(), 0, getPaddingRight(), triangleHeight);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (isInEditMode()) {
+            super.onDraw(canvas);
+            return;
+        }
+        RectF rectF = new RectF(0, 0, getMeasuredWidth(), getMeasuredHeight() - triangleHeight);
+        canvas.drawRoundRect(rectF, radius, radius, paint);
+        Path path = new Path();
+        int l = getMeasuredWidth() / 2 - triangleHeight / 2;
+        int r = l + triangleHeight;
+        int h = getMeasuredHeight() - triangleHeight;
+        path.moveTo(l, h);
+        path.lineTo(r, h);
+        path.lineTo((l + r) / 2, getMeasuredHeight());
+        path.lineTo(l, h);
+        path.close();
+        canvas.drawPath(path, paint);
+
+        setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        super.onDraw(canvas);
+    }
+
+}
