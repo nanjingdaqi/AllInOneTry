461bdba75777969205e3875074adf5908ee56f51
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-09-19 08:06:15 +0800

    try basic data trans

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package org.peace.allinone.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,27 +8,40 @@ import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import org.peace.allinone.R;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    @InjectView(R.id.start_btn)
-    Button mStartBtn;
+  @InjectView(R.id.start_btn) Button mStartBtn;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  int SIZE = 10000;
+  List<String> unit = new ArrayList<>();
 
-        ButterKnife.inject(this);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    ButterKnife.inject(this);
+
+    for (int i = 0; i < 100; ++i) {
+      unit.add("Hello" + i);
     }
+  }
 
-    @OnClick({R.id.start_btn})
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.start_btn) {
-
-        }
+  @OnClick({ R.id.start_btn }) public void onClick(View v) {
+    int id = v.getId();
+    if (id == R.id.start_btn) {
+      Intent intent = new Intent(this, SubActivity.class);
+      List<String> data = new LinkedList<>();
+      for (int i = 0; i < SIZE; ++i) {
+        data.addAll(unit);
+      }
+      intent.putExtra("TAG", (Serializable) data);
+      startActivity(intent);
     }
+  }
 }
---
+++ app/src/main/java/org/peace/allinone/ui/SubActivity.java
@@ -0,0 +1,8 @@
+package org.peace.allinone.ui;
+
+
+/**
+ * Created by peacepassion on 15/9/18.
+ */
+public class SubActivity extends MainActivity {
+}
