77e30b5243bbfce056815d32dd6bc7b0c89bc7ea
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-09-23 20:52:58 +0800

    try variable scope

--- app/src/debug/java/org/peace/try/hello.groovy
+++ app/src/debug/java/org/peace/try/hello.groovy
@@ -1,14 +1,9 @@
+import groovy.transform.Field;
 
+@Field x = 10;
 
-println 1.hashCode()
+def foo() {
+  println x
+}
 
-l = [1, 'hello']
-println l
-
-def m = [1: 100, 'hello': 'world']
-println m
-
-def r = 1..10
-def r2 = 1..<5
-println r
-println r2
+foo()
