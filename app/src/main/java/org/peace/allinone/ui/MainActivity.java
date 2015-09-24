531b62a26382d92840f522ebaff63b2b30fc1f85
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-09-24 19:29:51 +0800

    try basic expression

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import butterknife.InjectView;
-import butterknife.OnClick;
 import org.peace.allinone.R;
 import org.peace.allinone.databinding.ActivityMainBinding;
 
