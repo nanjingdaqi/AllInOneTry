b229db461241cae84f9c01c605558fb22b359677
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-09-25 17:29:11 +0800

    try reproduce cannot be visible, but fail

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -12,18 +13,39 @@ import org.peace.allinone.R;
 public class MainActivity extends AppCompatActivity {
 
   @InjectView(R.id.start_btn) Button mStartBtn;
+  @InjectView(R.id.tv) TextView tv;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+
+    runOnUiThread(new Runnable() {
+      @Override public void run() {
+        tv.setVisibility(View.GONE);
+        tv.setVisibility(View.GONE);
+      }
+    });
+
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
+      runOnUiThread(new Runnable() {
+        @Override public void run() {
+          tv.setVisibility(View.GONE);
+          tv.setVisibility(View.GONE);
+        }
+      });
 
+      runOnUiThread(new Runnable() {
+        @Override public void run() {
+          tv.setVisibility(View.GONE);
+          tv.setVisibility(View.VISIBLE);
+        }
+      });
     }
   }
 }
