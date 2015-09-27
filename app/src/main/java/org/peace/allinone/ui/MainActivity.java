209221ea3b03e6ee2294e7097d92e3e0a7f69b71
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-09-27 11:28:22 +0800

    finish basic smooth scroll for a view

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,8 +2,6 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import butterknife.ButterKnife;
@@ -15,30 +13,26 @@ import org.peace.allinone.R;
 public class MainActivity extends AppCompatActivity {
 
   @InjectView(R.id.start_btn) Button mStartBtn;
-  @InjectView(R.id.recyler_view) RecyclerView recyclerView;
+  @InjectView(R.id.my_view) MyView myView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    recyclerView.setAdapter(new RAdapter());
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
       logScroll();
-      //recyclerView.scrollBy(0, 100);
-      //recyclerView.smoothScrollBy(0, 100);
-      recyclerView.fling(0, 100);
+      myView.smoothScrollBy(100);
       logScroll();
     }
   }
 
   void logScroll() {
-    AppLogger.d("sx: " + recyclerView.getScrollX());
-    AppLogger.d("sy: " + recyclerView.getScrollY());
+    AppLogger.d("sx: " + myView.getScrollX());
+    AppLogger.d("sy: " + myView.getScrollY());
   }
 }
---
+++ app/src/main/java/org/peace/allinone/ui/MyView.java
@@ -0,0 +1,63 @@
+package org.peace.allinone.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Scroller;
+import android.widget.TextView;
+import me.ele.commons.DimenUtil;
+
+/**
+ * Created by peacepassion on 15/9/27.
+ */
+public class MyView extends View {
+
+  Paint textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+  Scroller scroller;
+
+
+  public MyView(Context context) {
+    super(context);
+    init();
+  }
+
+  public MyView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  private void init() {
+    textPaint.setColor(getResources().getColor(android.R.color.holo_blue_dark));
+    textPaint.setTextSize(DimenUtil.dip2px(getContext(), 20));
+    scroller = new Scroller(getContext());
+  }
+
+  @Override protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    int h = getMeasuredHeight(), y = 0, uh = DimenUtil.dip2px(getContext(), 20), i = 0;
+    while (y + uh / 2 < h) {
+      canvas.drawText("android " + i++, 0, y + uh / 2, textPaint);
+      y += uh;
+    }
+  }
+
+  public void smoothScrollBy(int dy) {
+    scroller.startScroll(0, getScrollY(), 0, dy, 3000);
+    invalidate();
+  }
+
+  @Override public void computeScroll() {
+    super.computeScroll();
+    if (scroller.computeScrollOffset()) {
+      scrollTo(0, scroller.getCurrY());
+      postInvalidate();
+    }
+  }
+}
