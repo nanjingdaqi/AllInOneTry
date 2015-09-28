162d56ecd5f0b15403901a26a586e043db41ff3f
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-09-28 17:47:23 +0800

    try customize binding

--- app/src/main/java/org/peace/allinone/ui/DataMgr.java
+++ app/src/main/java/org/peace/allinone/ui/DataMgr.java
@@ -1,7 +1,10 @@
 package org.peace.allinone.ui;
 
 import android.databinding.BaseObservable;
+import android.databinding.BindingAdapter;
 import android.databinding.ObservableField;
+import android.view.View;
+import me.ele.commons.AppLogger;
 
 /**
  * Created by peacepassion on 15/9/24.
@@ -9,5 +12,16 @@ import android.databinding.ObservableField;
 public class DataMgr extends BaseObservable {
 
   public ObservableField<String> data;
+
+  @BindingAdapter("bind:paddingLeft")
+  public static void setPaddingLeft(View v, int oldValue, int padding) {
+    AppLogger.d("old v: " + oldValue);
+    AppLogger.d("padding: " + padding);
+    v.setPadding(100, 100, 0, 0);
+  }
+
+  public int getPaddingLeft() {
+    return 100;
+  }
 
 }
