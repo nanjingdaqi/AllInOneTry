c628e077408efbd54bcd4e43b314be80d37aeb01
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-10-15 15:51:46 +0800

    register callback on post runnable

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+
+    mStartBtn.post(new Runnable() {
+      @Override public void run() {
+        mStartBtn.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+          @Override public void onViewAttachedToWindow(View v) {
+            AppLogger.d("attach");
+          }
+
+          @Override public void onViewDetachedFromWindow(View v) {
+            AppLogger.d("detach");
+          }
+        });
+      }
+    });
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
