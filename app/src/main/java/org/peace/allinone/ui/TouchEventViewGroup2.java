15e62364c2887bd1de290f46288702e3b0e53e10
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-10-24 20:33:09 +0800

    check dispatch event

--- app/src/main/java/org/peace/allinone/ui/TouchEventViewGroup2.java
+++ app/src/main/java/org/peace/allinone/ui/TouchEventViewGroup2.java
@@ -3,23 +3,27 @@ package org.peace.allinone.ui;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.widget.FrameLayout;
+import android.view.ViewGroup;
 import me.ele.commons.AppLogger;
 
 /**
  * Created by peacepassion on 15/10/24.
  */
-public class TouchEventViewGroup extends FrameLayout {
-  public TouchEventViewGroup(Context context) {
+public class TouchEventViewGroup2 extends ViewGroup {
+  public TouchEventViewGroup2(Context context) {
     super(context);
   }
 
-  public TouchEventViewGroup(Context context, AttributeSet attrs) {
+  public TouchEventViewGroup2(Context context, AttributeSet attrs) {
     super(context, attrs);
   }
 
-  public TouchEventViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+  public TouchEventViewGroup2(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+  }
+
+  @Override protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
   }
 
   @Override public boolean dispatchTouchEvent(MotionEvent ev) {
@@ -31,7 +35,7 @@ public class TouchEventViewGroup extends FrameLayout {
     AppLogger.d("onTouch event: " + event);
     int action = event.getAction();
     if (MotionEvent.ACTION_DOWN == action) {
-      return false;
+      return true;
     }
     return false;
   }
