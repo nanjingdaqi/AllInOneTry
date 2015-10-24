4ae3970e3923a4ee0ddbba2baa3f93ed3954caef
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-10-24 17:01:42 +0800

    use VelocityTracker for basic fling scroll view

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,19 +2,12 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.view.ViewConfiguration;
-import android.widget.Button;
 import butterknife.ButterKnife;
-import butterknife.InjectView;
-import butterknife.OnClick;
 import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
-
-  @InjectView(R.id.start_btn) Button mStartBtn;
-  @InjectView(R.id.my_view) View myView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -24,11 +17,5 @@ public class MainActivity extends AppCompatActivity {
 
     int slop = ViewConfiguration.get(this).getScaledTouchSlop();
     AppLogger.d("touch slop: " + slop);
-  }
-
-  @OnClick({ R.id.start_btn }) public void onClick(View v) {
-    int id = v.getId();
-    if (id == R.id.start_btn) {
-    }
   }
 }
---
+++ app/src/main/java/org/peace/allinone/ui/MyScrollView.java
@@ -0,0 +1,85 @@
+package org.peace.allinone.ui;
+
+import android.animation.TimeAnimator;
+import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.animation.LinearInterpolator;
+import android.widget.LinearLayout;
+import me.ele.commons.AppLogger;
+
+/**
+ * Created by peacepassion on 15/10/24.
+ */
+public class MyScrollView extends LinearLayout {
+
+  public MyScrollView(Context context) {
+    super(context);
+    init();
+  }
+
+  public MyScrollView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public MyScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  private void init() {
+
+  }
+
+  float ly;
+  VelocityTracker velocityTracker;
+  float dFlingSpd = 0.02f; // px/ms
+
+  @Override public boolean onTouchEvent(MotionEvent event) {
+    int action = event.getAction();
+    if (action == MotionEvent.ACTION_DOWN) {
+      ly = event.getY();
+      velocityTracker = VelocityTracker.obtain();
+      velocityTracker.addMovement(event);
+      return true;
+    }
+    velocityTracker.addMovement(event);
+    if (action == MotionEvent.ACTION_MOVE) {
+      for (int i = 0, hz = event.getHistorySize(); i < hz; ++i) {
+        //AppLogger.d("hz: " + hz);
+        float cy = event.getHistoricalY(i);
+        float dy = cy - ly;
+        scrollBy(0, -(int) dy);
+        ly = cy;
+      }
+      return true;
+    }
+    if (action == MotionEvent.ACTION_UP) {
+      velocityTracker.computeCurrentVelocity(1);
+      float yv = velocityTracker.getYVelocity();
+      AppLogger.w("fling speed: " + yv);
+      startFlingMove(yv);
+      return true;
+    }
+    return true;
+  }
+
+  private void startFlingMove(float yv) {
+    final float duration = Math.abs(yv) / dFlingSpd;
+    final int csy = getScrollY();
+    ValueAnimator va = TimeAnimator.ofFloat(0, 100).setDuration((long) (duration));
+    va.addUpdateListener(anim -> {
+      float frac = (float) anim.getAnimatedValue() / 100;
+      float t = duration * frac;
+      float dy = Math.abs(yv) * t - dFlingSpd * t * t / 2;
+      float ty = yv < 0 ? csy + dy : csy -dy;
+      AppLogger.d("t: " + t + ", dy: " + dy);
+      scrollTo(0, (int) ty);
+    });
+    va.start();
+  }
+}
