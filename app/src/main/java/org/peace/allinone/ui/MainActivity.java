765519681d996dc958a1c97374f619feb7656aa1
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-11-09 15:34:00 +0800

    check Dialog and Toast

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,13 +1,25 @@
 package org.peace.allinone.ui;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.jakewharton.rxbinding.view.RxView;
+import com.jakewharton.rxbinding.view.ViewClickEvent;
 import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
+import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
+
+  @InjectView(R.id.start) Button btn;
+  @InjectView(android.R.id.content) View cv;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -15,7 +27,13 @@ public class MainActivity extends AppCompatActivity {
 
     ButterKnife.inject(this);
 
-    int slop = ViewConfiguration.get(this).getScaledTouchSlop();
-    AppLogger.d("touch slop: " + slop);
+    Dialog dlg = new Dialog(this);
+    Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+
+    RxView.clickEvents(btn).subscribe(viewClickEvent -> {
+      WindowManager wm = getWindowManager();
+      wm.removeView(cv);
+    });
+
   }
 }
