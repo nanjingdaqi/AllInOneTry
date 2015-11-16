5011eb27b0e9bd4b621790292c1d2c68b4cac489
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-11-16 19:38:57 +0800

    check Gson format int

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -3,20 +3,36 @@ package org.peace.allinone.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
+import com.google.gson.Gson;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
+
+  @InjectView(R.id.start_btn) Button mStartBtn;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    ButterKnife.bind(this);
+    ButterKnife.inject(this);
   }
 
-  @OnClick(R.id.start_btn) public void onClick(View v) {
+  @OnClick({ R.id.start_btn }) public void onClick(View v) {
+    int id = v.getId();
+    if (id == R.id.start_btn) {
+      String json = "{ \"t\":1.232}";
+      Gson gson = new Gson();
+      T t = gson.fromJson(json, T.class);
+      AppLogger.d("t: " + t.t);
+    }
+  }
 
+  public static class T {
+    int t;
   }
 }
