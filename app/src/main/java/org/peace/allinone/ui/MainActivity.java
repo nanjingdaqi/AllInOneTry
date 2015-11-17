8c5b47d3056ad37aec03bbd834577e54af865bd2
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-11-17 18:09:14 +0800

    use AppCcompatActivity add android sdk fragment

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,39 +1,42 @@
 package org.peace.allinone.ui;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.start_btn)
-    Button mStartBtn;
-    @BindView(R.id.frag_container)
-    FrameLayout fragContainer;
+  @InjectView(R.id.start_btn) Button mStartBtn;
+  @InjectView(R.id.frag_container) FrameLayout fragContainer;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        ButterKnife.inject(this);
+    ButterKnife.inject(this);
+  }
+
+  @OnClick({ R.id.start_btn }) public void onClick(View v) {
+    int id = v.getId();
+    if (id == R.id.start_btn) {
+      MyFragment frag = new MyFragment();
+      getFragmentManager().beginTransaction().add(R.id.frag_container, frag).commit();
     }
+  }
 
-    @OnClick({R.id.start_btn})
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.start_btn) {
-            MyFragment frag = new MyFragment();
-            MyFragment frag2 = new MyFragment();
-            getSupportFragmentManager().beginTransaction().add(R.id.frag_container, frag, "TAG").commitAllowingStateLoss();
-//            getSupportFragmentManager().beginTransaction().add(R.id.frag_container, frag).commitAllowingStateLoss();
-            getSupportFragmentManager().executePendingTransactions();
-            Fragment target = getSupportFragmentManager().findFragmentByTag("TAG");
-            AppLogger.d("target == null: " + (target == null));
-        }
+  public static class MyFragment extends android.app.Fragment {
+
+    @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+      super.onCreateView(inflater, container, savedInstanceState);
+      return inflater.inflate(R.layout.frag_my, container, false);
     }
+  }
 }
