93a01d69137dc1d8bef7b8cffa4cb301073b3ca8
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-11-19 08:11:15 +0800

    try flatMap

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -7,13 +7,14 @@ import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
-import com.jakewharton.rxbinding.view.RxView;
-import com.jakewharton.rxbinding.view.ViewClickEvent;
-import com.jakewharton.rxbinding.widget.RxCompoundButton;
-import com.jakewharton.rxbinding.widget.RxTextView;
 import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
-import rx.functions.Action1;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +25,99 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+  }
 
-    RxView.clickEvents(mStartBtn).subscribe((event) -> {
-      View v = event.view();
-      AppLogger.d("view id: " + v.getId());
+  @OnClick(R.id.start_btn) public void onClick(View v) {
+    doTask(new MyCallback() {
+      @Override public void onSucc() {
+
+      }
+
+      @Override public void onFail(String s) {
+
+      }
     });
+  }
+
+  void doTask(MyCallback callback) {
+    Observable.create(new Observable.OnSubscribe<Void>() {
+      @Override public void call(Subscriber<? super Void> subscriber) {
+        task1(new MyCallback() {
+          @Override public void onSucc() {
+            subscriber.onNext(null);
+            subscriber.onCompleted();
+          }
+
+          @Override public void onFail(String s) {
+            subscriber.onError(new Exception(s));
+          }
+        });
+      }
+    })
+        .flatMap(new Func1<Void, Observable<Void>>() {
+          @Override public Observable<Void> call(Void aVoid) {
+            return Observable.create(new Observable.OnSubscribe<Void>() {
+              @Override public void call(Subscriber<? super Void> subscriber) {
+                task2(new MyCallback() {
+                  @Override public void onSucc() {
+                    subscriber.onNext(null);
+                    subscriber.onCompleted();
+                  }
+
+                  @Override public void onFail(String s) {
+                    subscriber.onError(new Exception(s));
+                  }
+                });
+              }
+            });
+          }
+        })
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Subscriber<Void>() {
+          @Override public void onStart() {
+            AppLogger.e("onStart ");
+          }
+
+          @Override public void onCompleted() {
+            AppLogger.e("onComplete ");
+          }
+
+          @Override public void onError(Throwable e) {
+
+            AppLogger.e("onError ");
+          }
+
+          @Override public void onNext(Void aVoid) {
+
+            AppLogger.e("onNext ");
+          }
+        });
+  }
+
+  void task1(MyCallback callback) {
+    try {
+      Thread.sleep(1000);
+      //callback.onSucc();
+      callback.onFail("fail1");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  void task2(MyCallback callback) {
+    try {
+      Thread.sleep(1000);
+      //callback.onSucc();
+      callback.onFail("fail2");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public interface MyCallback {
+    void onSucc();
+
+    void onFail(String s);
   }
 }
