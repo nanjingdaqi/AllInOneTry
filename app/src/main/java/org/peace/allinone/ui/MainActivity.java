57de69e4d3f48a98a06b55625532acf2bda95979
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-11-19 12:18:01 +0800

    try get window after activity has been destroyed

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -3,10 +3,12 @@ package org.peace.allinone.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,19 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+
+    mStartBtn.post(new Runnable() {
+      @Override public void run() {
+        Window window = getWindow();
+        View decor = window.getDecorView();
+        View content = window.findViewById(Window.ID_ANDROID_CONTENT);
+        AppLogger.e("content is: " + content);
+      }
+    });
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
