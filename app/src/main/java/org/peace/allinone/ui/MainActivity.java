1fc62bdcc48d635c3934b5cd3e1a4c4e3594b880
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-11-22 12:16:24 +0800

    try JRebel

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -3,7 +3,9 @@ package org.peace.allinone.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
-
+      Toast.makeText(this, "onCLick", Toast.LENGTH_SHORT).show();
     }
   }
 }
