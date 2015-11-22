16cc4dc0e0f25d67814752553c4031c3ab16adfc
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-11-22 13:15:01 +0800

    try basic draw circle

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,12 +1,18 @@
 package org.peace.allinone.ui;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +30,41 @@ public class MainActivity extends AppCompatActivity {
     int id = v.getId();
     if (id == R.id.start_btn) {
 
+    }
+  }
+
+  public static class MyView extends View {
+
+    Paint paint;
+
+    public MyView(Context context) {
+      super(context);
+      init();
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+      super(context, attrs);
+      init();
+    }
+
+    public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+      super(context, attrs, defStyleAttr);
+      init();
+    }
+
+    private void init() {
+      paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+      paint.setColor(getResources().getColor(android.R.color.holo_red_light));
+      paint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override protected void onDraw(Canvas canvas) {
+      super.onDraw(canvas);
+      int w = getMeasuredWidth(), h = getMeasuredHeight();
+      int r = 200;
+      canvas.drawCircle(w / 2, h / 2, r, paint);
+      Matrix mx = canvas.getMatrix();
+      AppLogger.d("canvas matrix: " + mx);
     }
   }
 }
