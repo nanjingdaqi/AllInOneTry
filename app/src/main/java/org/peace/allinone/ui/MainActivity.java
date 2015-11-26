e8ae0bded1f28d257ef660d463a8dd0d1f2d4112
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-11-26 19:32:34 +0800

    try Scan

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,17 +1,19 @@
 package org.peace.allinone.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 import butterknife.OnClick;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
-
-  @InjectView(R.id.start_btn) Button mStartBtn;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -20,10 +22,33 @@ public class MainActivity extends AppCompatActivity {
     ButterKnife.inject(this);
   }
 
-  @OnClick({ R.id.start_btn }) public void onClick(View v) {
+  @OnClick({ R.id.scan_btn, R.id.generate }) public void onClick(View v) {
     int id = v.getId();
-    if (id == R.id.start_btn) {
+    if (id == R.id.scan_btn) {
+      startScan();
+    }
+    if (id == R.id.generate) {
 
     }
+  }
+
+  private void startScan() {
+    IntentIntegrator integrator = new IntentIntegrator(this);
+    integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+    integrator.initiateScan();
+  }
+
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+    if (result == null) {
+      AppLogger.e("result is null");
+      return;
+    }
+    String content = result.getContents();
+    if (TextUtils.isEmpty(content)) {
+      AppLogger.e("content is null");
+      return;
+    }
+    AppLogger.d("scan result: " + content);
   }
 }
