31fd9e6d8d1cfa5f7a30aa250d97183dfbc11b8d
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-12-03 19:23:26 +0800

    check simple span and drawable

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,8 +2,12 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -12,12 +16,17 @@ import org.peace.allinone.R;
 public class MainActivity extends AppCompatActivity {
 
   @InjectView(R.id.start_btn) Button mStartBtn;
+  @InjectView(R.id.text_view) TextView textView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+
+    ImageSpan is = new ImageSpan(this, R.drawable.__leak_canary_icon);
+    SpannableString ss = new SpannableString(textView.getText());
+    ss.setSpan(is, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
