151851146c7197efacc32953045760a07d106e0e
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-12-11 21:34:18 +0800

    try use ScrollView to make disappear effect

---
+++ app/src/main/java/org/peace/allinone/ui/CartPopupContentLayout.java
@@ -0,0 +1,80 @@
+package org.peace.allinone.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import me.ele.commons.AppLogger;
+
+/**
+ * Created by peacepassion on 15/9/9.
+ */
+public class CartPopupContentLayout extends LinearLayout {
+
+  private View headView;
+  private ViewGroup listView;
+
+  private int maxHeight;
+  private int headViewHeight;
+  private int listViewItemHeight;
+
+  public CartPopupContentLayout(Context context) {
+    this(context, null);
+  }
+
+  public CartPopupContentLayout(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public CartPopupContentLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+
+    maxHeight = DimenUtil.dip2px(context, 390);
+    headViewHeight = DimenUtil.dip2px(context, 46);
+    listViewItemHeight = DimenUtil.dip2px(context, 64);
+  }
+
+  void setChildrenView(View headView, ViewGroup listView) {
+    this.headView = headView;
+    this.listView = listView;
+    invalidate();
+  }
+
+  @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    int w = MeasureSpec.getSize(widthMeasureSpec), h = MeasureSpec.getSize(heightMeasureSpec);
+    AppLogger.e("measured height: " + getMeasuredHeight());
+
+    if (headView != null && listView != null) {
+      headView.measure(MeasureSpec.makeMeasureSpec(w, MeasureSpec.EXACTLY),
+          MeasureSpec.makeMeasureSpec(headViewHeight, MeasureSpec.EXACTLY));
+
+      LayoutParams lp = new LayoutParams(w, headViewHeight);
+      headView.setLayoutParams(lp);
+
+      int num = 0;
+      if (listView instanceof ListView) {
+        ListView lv = (ListView) listView;
+        num = lv.getAdapter().getCount();
+      } else if (listView instanceof ScrollView) {
+        num = ((ViewGroup) listView.getChildAt(0)).getChildCount();
+      }
+      if (num * listViewItemHeight + headView.getMeasuredHeight() > maxHeight) {
+        //listView.measure(MeasureSpec.makeMeasureSpec(w, MeasureSpec.EXACTLY),
+        //    MeasureSpec.makeMeasureSpec(maxHeight - headViewHeight, MeasureSpec.EXACTLY));
+        lp.height = maxHeight - headViewHeight;
+      } else {
+        lp.height = num * listViewItemHeight;
+        //listView.measure(MeasureSpec.makeMeasureSpec(w, MeasureSpec.EXACTLY),
+        //    MeasureSpec.makeMeasureSpec(num * listViewItemHeight, MeasureSpec.EXACTLY));
+      }
+
+      listView.setLayoutParams(lp);
+    }
+
+    super.onMeasure(MeasureSpec.makeMeasureSpec(w, MeasureSpec.EXACTLY),
+        MeasureSpec.makeMeasureSpec(h, MeasureSpec.EXACTLY));
+  }
+}
--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -6,29 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import java.util.LinkedList;
-import java.util.List;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
   @BindView(R.id.start_btn) Button mStartBtn;
 
-  @BindView(R.id.list1) ListView list1;
-  @BindView(R.id.list2) ListView list2;
+  @BindView(R.id.scroll_view) ScrollView scrollView;
+  @BindView(R.id.ll) LinearLayout ll;
+  @BindView(R.id.root) LinearLayout root;
 
-  @InjectView(R.id.ll) LinearLayout ll;
-  @InjectView(R.id.root) LinearLayout root;
-
-  MyAdapter adapter1 = new MyAdapter();
   ArrayAdapter<String> adapter2;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.bind(this);
+
+    TextView head = new TextView(this);
+    head.setText("Head");
+    LinearLayout.LayoutParams lp =
+        new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+            DimenUtil.dip2px(this, 48));
+    head.setLayoutParams(lp);
+    //root.addView(head, 1);
+
+    //root.setChildrenView(head, scrollView);
 
     for (int i = 0; i < 10; ++i) {
       TextView tv = new TextView(this);
@@ -52,78 +57,11 @@ public class MainActivity extends AppCompatActivity {
               int h = (int) animation.getAnimatedValue();
               lp.height = h;
               tv.setLayoutParams(lp);
-              //tv.setTranslationY(h);
-              //tv.setY(y + h);
             }
           });
           va.setDuration(1000).start();
         }
       });
     }
-  }
-
-  @OnClick({ R.id.start_btn }) public void onClick(View v) {
-    int id = v.getId();
-    if (id == R.id.start_btn) {
-
-    }
-  }
-
-  class MyAdapter extends BaseAdapter {
-
-    List<String> contents;
-
-    MyAdapter() {
-      contents = new LinkedList<>();
-      for (int i = 0; i < 100; ++i) {
-        contents.add("item: " + i);
-      }
-    }
-
-    public void clearContents() {
-      contents.clear();
-    }
-
-    public void addContents() {
-      contents = new LinkedList<>();
-      for (int i = 0; i < 20; ++i) {
-        contents.add("Item: " + i);
-      }
-    }
-
-    @Override public int getCount() {
-      return contents.size();
-    }
-
-    @Override public String getItem(int position) {
-      return contents.get(position);
-    }
-
-    public void removeItem(int position) {
-      contents.remove(position);
-      notifyDataSetChanged();
-    }
-
-    @Override public long getItemId(int position) {
-      return position;
-    }
-
-    @Override public View getView(int position, View convertView, ViewGroup parent) {
-      VH vh;
-      if (convertView == null) {
-        convertView = getLayoutInflater().inflate(R.layout.item, parent, false);
-        vh = new VH();
-        vh.tv = (TextView) convertView.findViewById(R.id.tv);
-        convertView.setTag(vh);
-      } else {
-        vh = (VH) convertView.getTag();
-      }
-      vh.tv.setText(getItem(position));
-      return convertView;
-    }
-  }
-
-  class VH {
-    TextView tv;
   }
 }
