98c39c604d03f9c12083019365c65b3a492dc945
peacepassion <peacepassion@peacepassiondeMacBook-Pro.local>
2015-12-11 21:53:11 +0800

    try listview match_parent with parent gravity bottom

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -18,11 +18,8 @@ import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @BindView(R.id.start_btn) Button mStartBtn;
-
-  @BindView(R.id.scroll_view) ScrollView scrollView;
-  @BindView(R.id.ll) LinearLayout ll;
   @BindView(R.id.root) LinearLayout root;
+  @BindView(R.id.list_view) ListView listView;
 
   ArrayAdapter<String> adapter2;
 
@@ -32,36 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
     ButterKnife.bind(this);
 
-    TextView head = new TextView(this);
-    head.setText("Head");
-    LinearLayout.LayoutParams lp =
-        new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-            DimenUtil.dip2px(this, 48));
-    head.setLayoutParams(lp);
-    //root.addView(head, 1);
-
-    //root.setChildrenView(head, scrollView);
-
-    for (int i = 0; i < 10; ++i) {
-      TextView tv = new TextView(this);
-      tv.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 160));
-      tv.setText("Item " + i);
-      ll.addView(tv);
-      tv.setOnClickListener(new View.OnClickListener() {
-        @Override public void onClick(View v) {
-          ValueAnimator va = ValueAnimator.ofInt(tv.getHeight(), 0);
-          LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) tv.getLayoutParams();
-          float y = tv.getY();
-          va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override public void onAnimationUpdate(ValueAnimator animation) {
-              int h = (int) animation.getAnimatedValue();
-              lp.height = h;
-              tv.setLayoutParams(lp);
-            }
-          });
-          va.setDuration(1000).start();
-        }
-      });
+    adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+    for (int i = 0; i < 5; ++i) {
+      adapter2.add("Item : " + i);
     }
+    listView.setAdapter(adapter2);
+
   }
 }
