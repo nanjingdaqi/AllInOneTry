91effe49ee72be344c9eadda3d2b9c278c9eb25a
peacepassion <daqi1990@gmail.com>
2015-12-18 15:28:06 +0800

    try simple drawable left and drawable padding

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -30,14 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.inject(this);
 
-        String content = "( % 额配送会员 )";
-        SpannableString ss = new SpannableString(content);
-        Drawable vip = getResources().getDrawable(R.drawable.order_confirm_vip_tag);
-        vip.setBounds(0, 0, vip.getIntrinsicWidth(), vip.getIntrinsicHeight());
-        ImageSpan span = new ImageSpan(vip, ImageSpan.ALIGN_BASELINE);
-        int index = content.indexOf('%');
-        ss.setSpan(span, index, index + 1, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
-        textView.setText(ss);
     }
 
     @OnClick({R.id.start_btn})
