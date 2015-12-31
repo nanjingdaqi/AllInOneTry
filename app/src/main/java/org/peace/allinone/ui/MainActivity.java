9cc07e5a2bebddaca016e06e819d5948b500eab2
peacepassion <daqi1990@gmail.com>
2015-12-31 13:31:36 +0800

    try get Annotation value

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -4,35 +4,43 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 import butterknife.OnClick;
-import com.google.gson.Gson;
-import me.ele.commons.AppLogger;
+import com.google.gson.annotations.SerializedName;
+import me.ele.base.utils.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @InjectView(R.id.start_btn) Button mStartBtn;
+  @BindView(R.id.start_btn) Button mStartBtn;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    ButterKnife.inject(this);
+    ButterKnife.bind(this);
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
-      String json = "{ \"t\":1.2323e3}";
-      Gson gson = new Gson();
-      T t = gson.fromJson(json, T.class);
-      AppLogger.d("t: " + t.t);
+      try {
+
+        Target t = Target.OBJ;
+
+        String name = t.name();
+        SerializedName serializedName =
+            Target.class.getField(name).getAnnotation(SerializedName.class);
+        AppLogger.d("value: " + serializedName.value());
+      } catch (NoSuchFieldException e) {
+        e.printStackTrace();
+      }
     }
   }
 
-  public static class T {
-    int t;
+  enum Target {
+    @SerializedName("test")
+    OBJ
   }
 }
