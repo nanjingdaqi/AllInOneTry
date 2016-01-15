ad1d02f74fe8ee15e930c42c2337168746df7f30
peacepassion <daqi1990@gmail.com>
2016-01-15 15:02:07 +0800

    check GestureDetector onScroll

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,22 +2,46 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
   @InjectView(R.id.start_btn) Button mStartBtn;
+  @InjectView(R.id.my_layout) MyLayout myLayout;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+
+    GestureDetector gestureDetector = new GestureDetector(this, new SimpleOnGestureListener() {
+      @Override public boolean onDown(MotionEvent e) {
+        AppLogger.d("on down");
+        return true;
+      }
+
+      @Override
+      public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        AppLogger.d("dy: " + distanceY);
+        return false;
+      }
+    });
+
+    myLayout.setOnTouchListener(new View.OnTouchListener() {
+      @Override public boolean onTouch(View v, MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+      }
+    });
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
---
+++ app/src/main/java/org/peace/allinone/ui/MyLayout.java
@@ -0,0 +1,19 @@
+package org.peace.allinone.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class MyLayout extends LinearLayout {
+  public MyLayout(Context context) {
+    super(context);
+  }
+
+  public MyLayout(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public MyLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+}
