870b4f937773b8051bc845e374060e675af0490c
peacepassion <daqi1990@gmail.com>
2016-01-18 17:13:43 +0800

    try stateVisible of AndroidManifest.xml

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -4,14 +4,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
-import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,30 +28,24 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
-
-    boolean res = et1.requestFocus();
-    AppLogger.d("et1 request focus result: " + res);
-
-    imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-
-    //if (res) {
-      //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-      et1.postDelayed(new Runnable() {
-        @Override public void run() {
-         imm.showSoftInput(et1, InputMethodManager.SHOW_IMPLICIT);
-        }
-      }, 100);
-    //}
+    et1.append("Hello world");
   }
 
   @OnClick({ R.id.start_btn })
   public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
-      //if (et2.requestFocus()) {
-      //  //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-      //  imm.showSoftInput(et1, InputMethodManager.SHOW_IMPLICIT);
-      //}
+
     }
+  }
+
+  void showIM(final View v) {
+    imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    v.postDelayed(new Runnable() {
+      @Override public void run() {
+        v.requestFocus();
+        imm.showSoftInput(v, InputMethodManager.SHOW_IMPLICIT);
+      }
+    }, 100);
   }
 }
