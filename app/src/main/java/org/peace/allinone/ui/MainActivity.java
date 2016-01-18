12812227dc88f939ee85a5d7403ed3142d8caa9b
peacepassion <daqi1990@gmail.com>
2016-01-18 15:48:05 +0800

    try get focus and turn on input method

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -3,27 +3,45 @@ package org.peace.allinone.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
   @InjectView(R.id.start_btn) Button mStartBtn;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
+  @InjectView(R.id.et_1) EditText et1;
+
+  @InjectView(R.id.et_2) EditText et2;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+
+    boolean res = et1.requestFocus();
+    AppLogger.d("et1 request focus result: " + res);
+
+    if (res) {
+      getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+    }
   }
 
-  @OnClick({ R.id.start_btn }) public void onClick(View v) {
+  @OnClick({ R.id.start_btn })
+  public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
-
+      if (et2.requestFocus()) {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+      }
     }
   }
 }
