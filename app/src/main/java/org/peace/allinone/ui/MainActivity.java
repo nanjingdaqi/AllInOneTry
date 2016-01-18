accb674f134d50a42c62c68183d6efc355f4a8b8
peacepassion <daqi1990@gmail.com>
2016-01-18 16:59:10 +0800

    check show InputMethod

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,9 +1,11 @@
 package org.peace.allinone.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import butterknife.ButterKnife;
@@ -18,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
   @InjectView(R.id.et_1) EditText et1;
 
-  @InjectView(R.id.et_2) EditText et2;
+  //@InjectView(R.id.et_2) EditText et2;
+
+  private InputMethodManager imm;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +34,26 @@ public class MainActivity extends AppCompatActivity {
     boolean res = et1.requestFocus();
     AppLogger.d("et1 request focus result: " + res);
 
-    if (res) {
-      getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-    }
+    imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+    //if (res) {
+      //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+      et1.postDelayed(new Runnable() {
+        @Override public void run() {
+         imm.showSoftInput(et1, InputMethodManager.SHOW_IMPLICIT);
+        }
+      }, 100);
+    //}
   }
 
   @OnClick({ R.id.start_btn })
   public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
-      if (et2.requestFocus()) {
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-      }
+      //if (et2.requestFocus()) {
+      //  //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+      //  imm.showSoftInput(et1, InputMethodManager.SHOW_IMPLICIT);
+      //}
     }
   }
 }
