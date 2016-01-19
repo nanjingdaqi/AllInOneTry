8a3bb44b3d269bc539958a83415a269e0bfc3026
peacepassion <daqi1990@gmail.com>
2016-01-19 14:15:56 +0800

    check view onmeasure called times

---
+++ app/src/main/java/org/peace/allinone/ui/MyView.java
@@ -0,0 +1,26 @@
+package org.peace.allinone.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import me.ele.commons.AppLogger;
+
+public class MyView extends View {
+  public MyView(Context context) {
+    super(context);
+  }
+
+  public MyView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public MyView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    AppLogger.e(new Exception("onMeasure"));
+  }
+}
