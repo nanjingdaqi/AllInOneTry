82728e1c4f3ab124c14cca709f7564ed7dcace72
peacepassion <daqi1990@gmail.com>
2016-02-25 16:11:09 +0800

    check AndroidManifest hardware acceleration

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,5 +1,9 @@
 package org.peace.allinone.ui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,6 +12,8 @@ import android.widget.Button;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import com.squareup.okhttp.Interceptor;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,7 +31,22 @@ public class MainActivity extends AppCompatActivity {
     target.setVisibility(View.VISIBLE);
     target.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
       @Override public void onGlobalLayout() {
-        target.animate().translationXBy(300).setDuration(4000).start();
+        //target.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        ObjectAnimator anim = ObjectAnimator.ofFloat(target, "translationY", 0f, 300f);
+        anim.setDuration(4000);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+          @Override public void onAnimationUpdate(ValueAnimator animation) {
+            int layerType = target.getLayerType();
+            AppLogger.e("onUpdate layerT: " + getLayerType(layerType));
+            AppLogger.e("ishardware open: " + target.isHardwareAccelerated());
+          }
+        });
+        anim.addListener(new AnimatorListenerAdapter() {
+          @Override public void onAnimationEnd(Animator animation) {
+            target.setLayerType(View.LAYER_TYPE_NONE, null);
+          }
+        });
+        anim.start();
 
         target.getViewTreeObserver().removeGlobalOnLayoutListener(this);
       }
@@ -34,5 +55,17 @@ public class MainActivity extends AppCompatActivity {
 
   @OnClick(R.id.second_btn) public void onSecondBtn(View v) {
     target.animate().translationXBy(-300).setDuration(4000).start();
+  }
+
+  String getLayerType(int type) {
+    switch (type) {
+      case View.LAYER_TYPE_NONE:
+        return "none";
+      case View.LAYER_TYPE_SOFTWARE:
+        return "soft";
+      case View.LAYER_TYPE_HARDWARE:
+        return "hard";
+    }
+    return "";
   }
 }
