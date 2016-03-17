b3c88a5adeb46adc26c0c229f22067ed6237e822
peacepassion <daqi1990@gmail.com>
2016-03-17 15:41:20 +0800

    tmp

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,7 +1,11 @@
 package org.peace.allinone.ui;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -14,9 +18,33 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.bind(this);
+
+    logTheme("main activity", getTheme());
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
 
+  }
+
+  private void logTheme(String tag, Resources.Theme theme) {
+    TypedArray a = theme.obtainStyledAttributes(R.styleable.Theme);
+    boolean notitle = a.getBoolean(R.styleable.Theme_windowNoTitle, false);
+    Log.e(tag, "middle");
+    TypedValue tv = new TypedValue();
+    a.getValue(R.styleable.Theme_windowNoTitle, tv);
+    Log.e(tag, "tv: " + tv);
+    Log.e(tag, "middle");
+    boolean actionbar = a.getBoolean(R.styleable.Theme_windowActionBar, false);
+    Log.e(tag, "middle");
+    boolean abOverlay = a.getBoolean(R.styleable.Theme_windowActionBarOverlay, false);
+    a.recycle();
+    Log.e(tag, "theme id: " + R.styleable.Theme + ", notitle id: " + R.styleable.Theme_windowNoTitle);
+    Log.e(tag, "theme size: " + R.styleable.Theme.length);
+    String content = "[";
+    for (int i = 0; i < 10; i++) {
+      content += R.styleable.Theme[i] + ", ";
+    }
+    Log.e(tag, "theme 0-9: " + content + "]");
+    Log.e(tag, "notitle: " + notitle + ", actionbar: " + actionbar + ", abOverlay: " + abOverlay);
   }
 }
