5a63af474d6b61836f3a22c3038d917acc5f7f00
peacepassion <daqi1990@gmail.com>
2016-04-12 10:59:15 +0800

    fail to write /data/data/pkg/lib

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,6 +2,7 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -9,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import me.ele.commons.AppLogger;
+import okio.BufferedSink;
 import okio.BufferedSource;
 import okio.Okio;
 import org.peace.allinone.R;
@@ -17,13 +19,33 @@ public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-
-    ButterKnife.inject(this);
-
     try {
-      BufferedSource source = Okio.buffer(Okio.source(getAssets().open("peace.txt")));
-      AppLogger.e(source.readString(Charset.defaultCharset()));
+      setContentView(R.layout.activity_main);
+
+      ButterKnife.inject(this);
+
+      try {
+        BufferedSource source = Okio.buffer(Okio.source(getAssets().open("peace.txt")));
+        AppLogger.e(source.readString(Charset.defaultCharset()));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+      File lib = new File(getFilesDir().getParentFile(), "lib");
+      if (!lib.exists()) {
+        lib.mkdirs();
+      }
+
+      File test = new File(lib, "test.txt");
+      Log.e("MainActivity", test.getAbsolutePath());
+      if (!test.exists()) {
+        test.createNewFile();
+      }
+      BufferedSink sink = Okio.buffer(Okio.sink(test));
+      sink.writeUtf8("Hello world");
+      sink.flush();
+      sink.close();
+
     } catch (IOException e) {
       e.printStackTrace();
     }
