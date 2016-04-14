d23aa5d4f7ec569b522378004287e413c5afe26a
peacepassion <daqi1990@gmail.com>
2016-04-14 16:23:29 +0800

    try fitssystemwindow

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,10 +1,9 @@
 package org.peace.allinone.ui;
 
-import android.content.res.Resources;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -15,18 +14,29 @@ import me.ele.commons.AppLogger;
 import me.ele.commons.HexConverter;
 import org.peace.allinone.R;
 
-public class MainActivity extends AppCompatActivity  {
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
-    setTheme(R.style.MyAppTheme);
     super.onCreate(savedInstanceState);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      Window window = getWindow();
+
+      window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+          WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+    getWindow().getDecorView().setSystemUiVisibility(
+        View.SYSTEM_UI_FLAG_LOW_PROFILE |
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
-  }
-
-  @Override public Resources.Theme getTheme() {
-    AppLogger.e("get theme");
-    return super.getTheme();
+    setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
   }
 
   @Override public void setTheme(int resid) {
@@ -51,5 +61,6 @@ public class MainActivity extends AppCompatActivity  {
         t = p;
       }
     }
+    getSupportActionBar().setTitle("All in one try");
   }
 }
