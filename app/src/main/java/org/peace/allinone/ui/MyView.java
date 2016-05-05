ea89ec1b97cc8364aa2d11b7f3dd9c045770bd2e
peacepassion <daqi1990@gmail.com>
2016-05-05 17:51:34 +0800

    merge is root of included view cannot get the include view

--- app/src/main/java/org/peace/allinone/ui/MyView.java
+++ app/src/main/java/org/peace/allinone/ui/MyView.java
@@ -2,16 +2,19 @@ package org.peace.allinone.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MyView extends LinearLayout {
 
   @InjectView(R.id.sub_tv) TextView subTV;
+  @InjectView(R.id.my_sub) View includeView;
 
   public MyView(Context context, AttributeSet attrs) {
     super(context, attrs);
@@ -22,5 +25,6 @@ public class MyView extends LinearLayout {
 
   @OnClick(R.id.my_start_btn) public void onClick() {
     subTV.setText("New My_view_sub_tv");
+    AppLogger.e("includeView: " + includeView);
   }
 }
