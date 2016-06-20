eda1b71e609633f84d08e8fa837b8dc208ac759d
peacepassion <daqi1990@gmail.com>
2016-06-20 14:39:50 +0800

    test measuetext

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Layout;
+import android.text.TextPaint;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,21 +20,24 @@ public class MainActivity extends AppCompatActivity {
   @InjectView(R.id.start_btn) Button mStartBtn;
   @InjectView(R.id.tv) TextView tv;
 
+  TextPaint textPaint;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+
+    textPaint = new TextPaint();
+    textPaint.setTextSize(20);
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
-      Layout layout = tv.getLayout();
-      int ec = layout.getEllipsisCount(0);
-      AppLogger.e("ec: " + ec);
-      AppLogger.e("layout text: " + layout.getText());
-      AppLogger.e("textview text: " + tv.getText());
+      tv.setText("");
+      float w = textPaint.measureText(tv.getText().toString());
+      AppLogger.e("width: " + w);
     }
   }
 }
