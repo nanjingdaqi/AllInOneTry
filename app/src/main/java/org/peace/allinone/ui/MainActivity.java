93efc79323cb9f531101df83ccfa714f3cd3f76f
peacepassion <daqi1990@gmail.com>
2016-06-29 15:55:25 +0800

    try ColorMatrix

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,51 +1,45 @@
 package org.peace.allinone.ui;
 
-import android.graphics.Color;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import butterknife.ButterKnife;
 import butterknife.BindView;
-import butterknife.OnClick;
-import java.util.ArrayList;
-import java.util.List;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @BindView(R.id.food_icon) FoodIconView foodIconView;
+  @BindView(R.id.icon) ImageView iconView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     ButterKnife.bind(this);
-  }
 
-  @OnClick({ R.id.start_btn }) public void onClick(View v) {
-    int id = v.getId();
-    if (id == R.id.start_btn) {
-      List<FoodIconView.Icon> icons = new ArrayList<>(2);
-      icons.add(new FoodIconView.Icon() {
-        @Override public String text() {
-          return "特";
-        }
-
-        @Override public int color() {
-          return Color.BLACK;
-        }
-      });
-      icons.add(new FoodIconView.Icon() {
-        @Override public String text() {
-          return "新蔡";
-        }
-
-        @Override public int color() {
-          return Color.RED;
-        }
-      });
-      foodIconView.setIcons(icons);
-    }
+    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
+    Bitmap newBitmap =
+        Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), bitmap.getConfig());
+    Canvas canvas = new Canvas(newBitmap);
+    Paint paint = new Paint();
+    float[] src = new float[] {
+        0.5f, 0, 0, 0, 0,
+        0, 0.5f, 0, 0, 0,
+        0, 0, 0.5f, 0, 0,
+        0, 0, 0, 1, 0
+    };
+    ColorMatrix cm = new ColorMatrix(src);
+    paint.setColorFilter(new ColorMatrixColorFilter(cm));
+    canvas.drawBitmap(bitmap, new Matrix(), paint);
+    iconView.setImageBitmap(newBitmap);
   }
 }
