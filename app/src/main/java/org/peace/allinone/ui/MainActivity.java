19a3b79b890aa99bf50de389fce775ca3e3b4733
peacepassion <daqi1990@gmail.com>
2016-07-14 09:40:17 +0800

    check ellipsize on text padding

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,10 +1,7 @@
 package org.peace.allinone.ui;
 
-import android.app.Application;
-import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
 import android.text.TextPaint;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +9,6 @@ import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
-import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,17 +23,9 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
-
-    textPaint = new TextPaint();
-    textPaint.setTextSize(20);
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
-    int id = v.getId();
-    if (id == R.id.start_btn) {
-      tv.setText("");
-      float w = textPaint.measureText(tv.getText().toString());
-      AppLogger.e("width: " + w);
-    }
+
   }
 }
