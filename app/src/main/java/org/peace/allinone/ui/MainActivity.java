e35de90660a0c0e4b4dd2e13342509b16ab5c7f6
peacepassion <daqi1990@gmail.com>
2016-08-03 17:41:16 +0800

    simple matrix

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,10 +1,12 @@
 package org.peace.allinone.ui;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
+    Matrix matrix = new Matrix();
+    AppLogger.e("m0: " + matrix);
+    matrix.preTranslate(10, 10);
+    AppLogger.e("m1: " + matrix);
+    matrix.preRotate(90);
+    AppLogger.e("m2: " + matrix);
   }
 }
