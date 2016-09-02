e12b957f5a842768f476cb4d4ecc18af79c5dab7
peacepassion <daqi1990@gmail.com>
2016-09-02 16:05:33 +0800

    check same res in the app and library

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,16 +1,19 @@
 package org.peace.allinone.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
+import com.example.mylibrary.TestActivity;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
   @InjectView(R.id.start_btn) Button mStartBtn;
-  @InjectView(R.id.target) View target;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -20,6 +23,6 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
+    startActivity(new Intent(this, TestActivity.class));
   }
 }
