c870370e2b55d5ae22cdc61a2af137b31c0464e8
peacepassion <daqi1990@gmail.com>
2016-09-06 17:24:30 +0800

    check app lib path

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,10 +1,16 @@
 package org.peace.allinone.ui;
 
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import dalvik.system.BaseDexClassLoader;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import me.ele.commons.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +20,30 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.inject(this);
+
+    String javaLibraryPath = System.getProperty("java.library.path");
+    AppLogger.e("java lib path: " + javaLibraryPath);
+
+    BaseDexClassLoader classLoader = (BaseDexClassLoader) getClassLoader();
+    try {
+      Method method = BaseDexClassLoader.class.getMethod("getLdLibraryPath");
+      String path = (String) method.invoke(classLoader);
+      AppLogger.e("lib path: " + path);
+
+      ApplicationInfo applicationInfo = getApplicationInfo();
+      AppLogger.e("app info lib path: " + applicationInfo.nativeLibraryDir);
+      AppLogger.e("app info app dir: " + applicationInfo.sourceDir);
+      Field field = ApplicationInfo.class.getDeclaredField("primaryCpuAbi");
+      field.setAccessible(true);
+      String primaryCpuAbi = (String) field.get(applicationInfo);
+      AppLogger.e("app info primary abi: " + primaryCpuAbi);
+
+      field = Application.class.getDeclaredField("mLoadedApk");
+      field.setAccessible(true);
+      Object loadedApk = field.get(getApplication());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
