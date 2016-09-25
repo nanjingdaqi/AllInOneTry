37dab4a5fe37d3d47466d796559fd07903d40697
peacepassion <daqi1990@gmail.com>
2016-09-25 17:23:26 +0800

    test device_id"

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -3,11 +3,15 @@ package org.peace.allinone.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
+
+  @InjectView(R.id.tv) TextView tv;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
+    String deviceId = DeviceId.getDeviceId(getApplication());
+    tv.setText(deviceId);
   }
 }
