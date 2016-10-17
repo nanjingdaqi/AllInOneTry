3299f15262287de06996103223700c8b42a02e46
peacepassion <daqi1990@gmail.com>
2016-10-17 20:04:58 +0800

    setup test env

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,22 +1,55 @@
 package org.peace.allinone.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.View;
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
+import me.ele.commons.DimenUtil;
+import me.ele.components.pullrefresh.PullToRefresh;
+import me.ele.components.recyclerview.EMRecyclerView;
 import org.peace.allinone.R;
 
+import static me.ele.commons.DimenUtil.dip2px;
+import static org.peace.allinone.ui.Util.createHead;
+
 public class MainActivity extends AppCompatActivity {
+
+  static final String TAG = MainActivity.class.getSimpleName();
+
+  int H;
+
+  @InjectView(R.id.rv) EMRecyclerView rv;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    H = dip2px(this, 60);
 
     ButterKnife.inject(this);
+    setupRV();
   }
 
-  @OnClick(R.id.start_btn) public void onClick(View v) {
+  public void setupRV() {
+    rv.setLayoutManager(new LinearLayoutManager(this));
+    Util.insertItem(this, rv);
+    rv.addHeaderView(createHead(this, H, Color.RED, "Head 1"));
+    rv.addHeaderView(createHead(this, H, Color.BLUE, "Head 2"));
+    rv.setRefreshListener(new PullToRefresh.OnRefreshListener() {
+      @Override public void onRefresh() {
+        Log.e(TAG, "onRefresh");
+        rv.postDelayed(new Runnable() {
+          @Override public void run() {
+            rv.finishRefresh();
+          }
+        }, 3000);
+      }
+    });
+    rv.enablePullRefresh();
 
   }
 }
---
+++ app/src/main/java/org/peace/allinone/ui/Util.java
@@ -0,0 +1,65 @@
+package org.peace.allinone.ui;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.widget.TextView;
+import me.ele.components.recyclerview.EMRecyclerView;
+import me.ele.components.recyclerview.HeaderViewRecyclerAdapter;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static me.ele.commons.DimenUtil.dip2px;
+
+public class Util {
+
+  public static TextView createHead(Context context, int h, int bg, String text) {
+    TextView tv = new TextView(context);
+    tv.setLayoutParams(new MarginLayoutParams(MATCH_PARENT, h));
+    tv.setText(text);
+    tv.setBackgroundColor(bg);
+    tv.setGravity(Gravity.CENTER);
+    tv.setTextSize(30);
+    return tv;
+  }
+
+  public static void insertItem(Context context, EMRecyclerView recyclerView) {
+    recyclerView.setAdapter(new HeaderViewRecyclerAdapter(new MyAdapter()) {
+      @Override public boolean isEmpty() {
+        return false;
+      }
+    });
+  }
+
+  public static class MyVH extends RecyclerView.ViewHolder {
+
+    TextView tv;
+
+    public MyVH(View itemView) {
+      super(itemView);
+      this.tv = (TextView) itemView;
+    }
+  }
+
+  public static class MyAdapter extends RecyclerView.Adapter<MyVH> {
+
+    @Override public MyVH onCreateViewHolder(ViewGroup parent, int viewType) {
+      TextView textView = new TextView(parent.getContext());
+      textView.setLayoutParams(
+          new MarginLayoutParams(MATCH_PARENT, dip2px(parent.getContext(), 40)));
+      textView.setGravity(Gravity.CENTER);
+      textView.setTextSize(20);
+      return new MyVH(textView);
+    }
+
+    @Override public void onBindViewHolder(MyVH holder, int position) {
+      holder.tv.setText("Item " + position);
+    }
+
+    @Override public int getItemCount() {
+      return 20;
+    }
+  }
+}
