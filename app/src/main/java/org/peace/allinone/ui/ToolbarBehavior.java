13d0c9417cd9b75704650d05e231952b50283271
peacepassion <daqi1990@gmail.com>
2016-10-18 16:54:18 +0800

    add fling support

--- app/src/main/java/org/peace/allinone/ui/ToolbarBehavior.java
+++ app/src/main/java/org/peace/allinone/ui/ToolbarBehavior.java
@@ -3,6 +3,9 @@ package org.peace.allinone.ui;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ScrollerCompat;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -51,7 +54,7 @@ public class ToolbarBehavior extends CoordinatorLayout.Behavior<View> {
   @Override public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target,
       int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
     Log.e(TAG, "onNestedScroll, dyUnconsumed: " + dyUnconsumed);
-    int maxH = DimenUtil.dip2px(child.getContext(), 100);
+    int maxH = DimenUtil.dip2px(child.getContext(), 300);
 
     // down scroll
     if (dyUnconsumed < 0) {
@@ -61,9 +64,72 @@ public class ToolbarBehavior extends CoordinatorLayout.Behavior<View> {
     }
   }
 
-  @Override public void onNestedScrollAccepted(CoordinatorLayout coordinatorLayout, View child,
-      View directTargetChild, View target, int nestedScrollAxes) {
-    super.onNestedScrollAccepted(coordinatorLayout, child, directTargetChild, target,
-        nestedScrollAxes);
+  private ScrollerCompat mScroller;
+  private FlingRunnable mFlingRunnable;
+
+  @Override
+  public boolean onNestedPreFling(CoordinatorLayout coordinatorLayout, View child, View target,
+      float velocityX, float velocityY) {
+    Log.e(TAG, "onNestedPreFling, vy: " + velocityY);
+    if (mScroller == null) {
+      mScroller = ScrollerCompat.create(child.getContext());
+    }
+    // up fling
+    if (velocityY > 0 && child.getLayoutParams().height > DimenUtil.dip2px(child.getContext(), 40)) {
+      mScroller.fling(0, (int) target.getY(), 0, (int) (-velocityY), 0, 0, DimenUtil.dip2px(child.getContext(), 40), DimenUtil.dip2px(child.getContext(), 300));
+      if (mScroller.computeScrollOffset()) {
+        mFlingRunnable = new FlingRunnable(child, target);
+        ViewCompat.postOnAnimation(child, mFlingRunnable);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  @Override
+  public boolean onNestedFling(CoordinatorLayout coordinatorLayout, View child, View target,
+      float velocityX, float velocityY, boolean consumed) {
+    Log.e(TAG, "onNestedFling, vy: " + velocityY);
+    if (mScroller == null) {
+      mScroller = ScrollerCompat.create(child.getContext());
+    }
+
+    // down fling
+    RecyclerView rv = (RecyclerView) target;
+    LinearLayoutManager llm = (LinearLayoutManager) rv.getLayoutManager();
+    if (velocityY < 0 && llm.findFirstVisibleItemPosition() == 0 && child.getLayoutParams().height < DimenUtil.dip2px(child.getContext(), 300)) {
+      mScroller.fling(0, (int) target.getY(), 0, (int) (-velocityY), 0, 0, DimenUtil.dip2px(child.getContext(), 40), DimenUtil.dip2px(child.getContext(), 300));
+      if (mScroller.computeScrollOffset()) {
+        mFlingRunnable = new FlingRunnable(child, target);
+        ViewCompat.postOnAnimation(child, mFlingRunnable);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private class FlingRunnable implements Runnable {
+
+    View child;
+    View target;
+    int minH;
+
+    public FlingRunnable(View child, View target) {
+      this.child = child;
+      this.target = target;
+      minH = DimenUtil.dip2px(child.getContext(), 40);
+    }
+
+    @Override public void run() {
+      if (mScroller.computeScrollOffset()) {
+        ViewGroup.LayoutParams lp = child.getLayoutParams();
+        int value = mScroller.getCurrY();
+        lp.height = Math.max(minH, value);
+        child.setLayoutParams(lp);
+        ViewCompat.postOnAnimation(child, this);
+      }
+    }
   }
 }
