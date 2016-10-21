60a9118c917750d74475a37c7f8b985dfe0480a6
peacepassion <daqi1990@gmail.com>
2016-10-21 10:26:06 +0800

    test y, ty, top info

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,12 +2,17 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import me.ele.base.utils.DimenUtil;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.target) View targetView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
+    logTargetInfo();
+    targetView.setY(30);
+    logTargetInfo();
+  }
 
+  void logTargetInfo() {
+    String log = "y: " + targetView.getY() + "\n"
+        + "ty: " + targetView.getTranslationY() + "\n"
+        + "top: " + targetView.getTop();
+    Log.e("Peace", log);
   }
 }
