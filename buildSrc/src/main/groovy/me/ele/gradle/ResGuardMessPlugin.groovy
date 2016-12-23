99b708dacebd9079e31643ca206055c3a3b68af9
peacepassion <daqi1990@gmail.com>
2016-12-23 15:33:03 +0800

    append res guard white list

--- buildSrc/src/main/groovy/me/ele/gradle/ResGuardMessPlugin.groovy
+++ buildSrc/src/main/groovy/me/ele/gradle/ResGuardMessPlugin.groovy
@@ -1,17 +1,16 @@
 package me.ele.gradle
 
-import com.android.build.gradle.api.ApkVariant
-import com.android.build.gradle.api.LibraryVariant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.api.tasks.compile.JavaCompile
 
 public class ResGuardMessPlugin implements Plugin<Project> {
 
   @Override
   void apply(Project project) {
-
+    project.afterEvaluate {
+      println(project.andResGuard.whiteList.toString())
+      project.andResGuard.whiteList.add('R.layout.activity_main')
+      println(project.andResGuard.whiteList.toString())
+    }
   }
-
 }
