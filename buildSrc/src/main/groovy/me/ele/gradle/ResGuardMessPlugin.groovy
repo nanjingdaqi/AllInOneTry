4a8780dde93c4b53c53dc1d6ad1141277b274429
peacepassion <daqi1990@gmail.com>
2016-12-23 14:58:32 +0800

    introduce plugin

---
+++ buildSrc/src/main/groovy/me/ele/gradle/ResGuardMessPlugin.groovy
@@ -0,0 +1,17 @@
+package me.ele.gradle
+
+import com.android.build.gradle.api.ApkVariant
+import com.android.build.gradle.api.LibraryVariant
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.api.tasks.compile.JavaCompile
+
+public class ResGuardMessPlugin implements Plugin<Project> {
+
+  @Override
+  void apply(Project project) {
+
+  }
+
+}
