faefb8d1466bf6aaa3d89480ffcb835047e00a38
peacepassion <daqi1990@gmail.com>
2016-12-26 14:20:01 +0800

    try get all dependencies introduced by one dependency

--- buildSrc/src/main/groovy/org/peace/MyPlugin.groovy
+++ buildSrc/src/main/groovy/org/peace/MyPlugin.groovy
@@ -4,6 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.result.DependencyResult
 import org.gradle.api.artifacts.result.ResolutionResult
+import org.gradle.api.artifacts.result.ResolvedDependencyResult
 
 public class MyPlugin implements Plugin<Project> {
 
@@ -16,10 +17,51 @@ public class MyPlugin implements Plugin<Project> {
           getIncoming().
           getResolutionResult()
 
-      resolutionResult.allDependencies.each { DependencyResult dr ->
-        println "name: ${dr.getRequested().displayName}"
-        println "from id: ${dr.getFrom().id}"
+      //      resolutionResult.allDependencies.each { DependencyResult dr ->
+      //        println "name: ${dr.getRequested().displayName}"
+      //        println "from id: ${dr.getFrom().id}"
+      //      }
+      //
+      //      println '----------------'
+
+      ((ResolvedDependencyResult) resolutionResult.allDependencies.find {
+        DependencyResult dr -> return dr.getRequested().displayName.contains('retrofit')
+      }).getSelected().
+          getDependencies().
+          each { DependencyResult dr2 -> println(dr2.requested.displayName)
+          }
+
+      printSep()
+
+      ((ResolvedDependencyResult) resolutionResult.allDependencies.find {
+        DependencyResult dr -> return dr.getRequested().displayName.contains('pay-warlock')
+      }).getSelected().
+          getDependents().
+          each { ResolvedDependencyResult rdr -> println(rdr.requested.displayName)
+          }
+
+      printSep()
+
+      getAllDependencies(resolutionResult.allDependencies.find {
+        DependencyResult dr -> return dr.getRequested().displayName.contains('pay-core')
+      }).each { DependencyResult dr -> println(dr.requested.displayName)
       }
     }
+  }
+
+  private Set<DependencyResult> getAllDependencies(DependencyResult dr) {
+    Set<DependencyResult> results = new HashSet<>()
+    Set<DependencyResult> tmpResults = ((ResolvedDependencyResult) dr).getSelected().
+        getDependencies()
+    if (tmpResults != null && tmpResults.size() > 0) {
+      results.addAll(tmpResults)
+      tmpResults.each { DependencyResult dr2 -> results.addAll(getAllDependencies(dr2))
+      }
+    }
+    return results
+  }
+
+  private void printSep() {
+    println '---------------------------------'
   }
 }
