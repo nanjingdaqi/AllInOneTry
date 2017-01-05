0a2a66fc701975e262a4defae8f15c8ebea51bc9
peacepassion <daqi1990@gmail.com>
2017-01-05 12:12:58 +0800

    try map

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -7,13 +7,17 @@ import android.widget.Button;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
+import java.util.Map;
 import me.ele.base.utils.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
   @BindView(R.id.start_btn) Button mStartBtn;
+
+  static String jsonStr = "{\"key\": {\"k1\": {\"one\": 1, \"two\": 2}, \"k2\": {\"one\": 10, \"two\": 20}}}";
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -23,24 +27,22 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
-    int id = v.getId();
-    if (id == R.id.start_btn) {
-      try {
-
-        Target t = Target.OBJ;
-
-        String name = t.name();
-        SerializedName serializedName =
-            Target.class.getField(name).getAnnotation(SerializedName.class);
-        AppLogger.d("value: " + serializedName.value());
-      } catch (NoSuchFieldException e) {
-        e.printStackTrace();
-      }
-    }
+    Gson gson = new Gson();
+    M m = gson.fromJson(jsonStr, M.class);
+    AppLogger.e("" + m);
   }
 
   enum Target {
     @SerializedName("test")
     OBJ
+  }
+
+  public static class M {
+    @SerializedName("key") public Map<String, T> map;
+  }
+
+  public static class T {
+    @SerializedName("one") public int one;
+    @SerializedName("two") public int two;
   }
 }
