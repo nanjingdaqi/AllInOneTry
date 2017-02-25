06c9365f72ce044831b1ff9d3b7134e1940cfabe
daqi <daqi@xiaomi.com>
2017-02-25 19:09:03 +0800

    simple content resolver

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,13 +1,20 @@
 package org.peace.allinone.ui;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.UserDictionary;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.list_view) ListView listView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -18,5 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
 
+    String[] cols = { UserDictionary.Words._ID, UserDictionary.Words.WORD, UserDictionary.Words.LOCALE };
+    Cursor cursor =
+        getContentResolver().query(UserDictionary.Words.CONTENT_URI, cols, null, null, null);
+    String[] cols2 = {UserDictionary.Words.WORD, UserDictionary.Words.LOCALE };
+    int[] ids = { R.id.word, R.id.local };
+    SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.layout_cell, cursor, cols2, ids);
+    listView.setAdapter(adapter);
   }
 }
