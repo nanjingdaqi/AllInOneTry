35c887c8aeb6c3332c6abed15e11c218c0624691
daqi <daqi@xiaomi.com>
2017-03-02 09:58:41 +0800

    check locale construction

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import java.util.Locale;
+import me.ele.base.utils.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
+    Locale l1 = new Locale("zh", "cn");
+    AppLogger.e("l1: " + l1);
+    Locale l2 = new Locale("zh_CN");
+    AppLogger.e("l2: " + l2);
+    Locale l3 = new Locale(l1.toString());
+    AppLogger.e("l3: " + l3);
   }
 }
