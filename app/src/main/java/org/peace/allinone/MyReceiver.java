520a0bfda23e0789ecf794afcbd070fc97f740a0
daqi <daqi@xiaomi.com>
2017-03-07 09:27:45 +0800

    simple try

---
+++ app/src/main/java/org/peace/allinone/MyReceiver.java
@@ -0,0 +1,16 @@
+package org.peace.allinone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by daqi on 17-3-6.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+
+  @Override public void onReceive(Context context, Intent intent) {
+
+  }
+}
