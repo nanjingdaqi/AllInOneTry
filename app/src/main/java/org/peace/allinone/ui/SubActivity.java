cbf445f45b87731feb7331a17494ecb3e8487ef3
daqi <daqi@xiaomi.com>
2017-03-08 17:58:11 +0800

    check compoenent enable status

--- app/src/main/java/org/peace/allinone/ui/SubActivity.java
+++ app/src/main/java/org/peace/allinone/ui/SubActivity.java
@@ -1,6 +1,5 @@
 package org.peace.allinone.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,7 +7,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -18,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-    startActivity(new Intent(this, SubActivity.class));
+
   }
 }
