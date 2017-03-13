67e50e6add50d181ed6154dfcfd45bef8f0612a3
daqi <daqi@xiaomi.com>
2017-03-13 14:46:28 +0800

    check class hashcode and class name hashcode

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import me.ele.base.utils.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
+    AppLogger.e("class hash code: " + getClass().hashCode());
+    AppLogger.e("classname hash code: " + getClass().getName().hashCode());
   }
 }
