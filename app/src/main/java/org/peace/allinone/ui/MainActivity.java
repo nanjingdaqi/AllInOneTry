68052f6c6f098df3218649d61541bfee63eb4396
daqi <daqi@xiaomi.com>
2017-04-07 16:22:51 +0800

    check heap size and OOM

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -9,6 +9,11 @@ import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
+  byte[] bytes;
+  byte[] bytes2;
+  byte[] byte3;
+  byte[] bytes4;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
@@ -17,6 +22,21 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
+    if (bytes == null) {
+      bytes = new byte[10 * 1024 * 1024];
+      return;
+    }
+    if (bytes2 == null) {
+      bytes2 = new byte[10 * 1024 * 1024];
+      return;
+    }
+    if (byte3 == null) {
+      byte3 = new byte[200 * 1024 * 1024];
+      return;
+    }
+    if (bytes4 == null) {
+      bytes4 = new byte[600 * 1024 * 1024];
+      return;
+    }
   }
 }
