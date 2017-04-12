374f2d2c7fa5094ed2c4ae9d37e1dcb984caf535
daqi <daqi@xiaomi.com>
2017-04-12 14:21:09 +0800

    check context and app context, and system service

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import me.ele.base.utils.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,9 +15,15 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     ButterKnife.bind(this);
+    AppLogger.e("me: " + this);
+    AppLogger.e("app: " + this.getApplication());
+    AppLogger.e("me app context: " + getApplicationContext());
+    AppLogger.e("app app context: " + getApplication().getApplicationContext());
+
+    AppLogger.e("me wifi: " + getSystemService(WIFI_SERVICE));
+    AppLogger.e("app wifi: " + getApplication().getSystemService(WIFI_SERVICE));
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
   }
 }
