15004a1c75f0ceda518830b85149ed9ec0213264
daqi <daqi@xiaomi.com>
2017-05-29 11:43:05 +0800

    check Systrace && TraceView

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,8 +1,11 @@
 package org.peace.allinone.ui;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.os.Trace;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
@@ -17,6 +20,27 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
+    Trace.beginSection("Peace");
+    Debug.startMethodTracing("peace2");
+    doSth();
+    Debug.stopMethodTracing();
+    Trace.endSection();
+  }
 
+  void doSth() {
+    doSth2();
+    do3();
+  }
+
+  void doSth2() {
+    Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show();
+  }
+
+  void do3() {
+    try {
+      Thread.sleep(1 * 1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
