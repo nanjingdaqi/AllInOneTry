f686c48b1996ba4e4cd3e6666340fce5fb246b2c
daqi <daqi@xiaomi.com>
2017-06-01 10:51:45 +0800

    check static inner class default constructor

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package org.peace.allinone.ui;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
@@ -20,5 +21,9 @@ public class MainActivity extends AppCompatActivity {
   @OnClick(R.id.start_btn) public void onClick(View v) {
     AppLogger.e("class hash code: " + getClass().hashCode());
     AppLogger.e("classname hash code: " + getClass().getName().hashCode());
+  }
+
+  public static class StaticClass extends DialogFragment {
+
   }
 }
