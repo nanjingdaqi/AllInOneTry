f41c5bcccbb358051b1015fc1f681918cc5255e2
daqi <daqi@xiaomi.com>
2017-06-01 22:46:42 +0800

    half done

---
+++ app/src/main/java/org/peace/allinone/ui/T.java
@@ -0,0 +1,24 @@
+package org.peace.allinone.ui;
+
+/**
+ * Created by daqi on 17-8-7.
+ */
+
+public class T {
+
+    public static final String T = "daqi";
+
+    public final int i = 0;
+
+    public T() {
+
+    }
+
+    public static final String sf(int arg) {
+        return "sf";
+    }
+
+    public final String f() {
+        return "foo";
+    }
+}
