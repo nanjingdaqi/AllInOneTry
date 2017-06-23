5ae3db378090e4997bc390a980967452eee1b5a4
daqi <daqi@xiaomi.com>
2017-06-23 21:26:49 +0800

    check reflection change final field

---
+++ app/src/main/java/org/peace/allinone/ui/A.java
@@ -0,0 +1,9 @@
+package org.peace.allinone.ui;
+
+/**
+ * Created by daqi on 17-6-23.
+ */
+
+public class A {
+    public int m = -1;
+}
--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,12 +2,19 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
 
+import java.lang.reflect.Field;
+
 public class MainActivity extends AppCompatActivity {
+
+  final Object obj = new Object();
+    final int i = 100;
+  final A a = new A();
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
+//      Log.d("Peace", "obj: " + obj);
+    Log.d("Peace", "i: " + i);
+      Log.d("Peace", "a m: " + a.m);
+    try {
+      Field f = MainActivity.class.getDeclaredField("a");
+      f.setAccessible(true);
+      A a2 = new A();
+      a2.m = 200;
+      f.set(this, a2);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    Log.d("Peace", "a m2: " + a.m);
   }
 }
