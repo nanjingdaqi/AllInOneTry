0ff49c8013eec82df0b6298fae13fc4d5e3a3ebe
daqi <daqi@xiaomi.com>
2017-06-29 13:38:51 +0800

    check AsyncTask cancel op

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,22 +1,51 @@
 package org.peace.allinone.ui;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import me.ele.base.utils.AppLogger;
 import org.peace.allinone.R;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    ButterKnife.bind(this);
-  }
+        ButterKnife.bind(this);
+        task = new MyTask();
+        task.execute();
+    }
 
-  @OnClick(R.id.start_btn) public void onClick(View v) {
+    MyTask task;
 
-  }
+    @OnClick(R.id.start_btn)
+    public void onClick(View v) {
+        task.cancel(true);
+    }
+
+    public class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Log.e("Peace", "doInBackground");
+            long cur = System.currentTimeMillis();
+            while (System.currentTimeMillis() < cur + 20 * 1000) {
+
+            }
+            Log.e("Peace", "end");
+            return null;
+        }
+
+        @Override
+        protected void onCancelled(Void aVoid) {
+            super.onCancelled(aVoid);
+            Log.e("Peace", "onCancelled");
+        }
+    }
 }
