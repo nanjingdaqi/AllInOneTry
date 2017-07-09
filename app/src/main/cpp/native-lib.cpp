788a9bcbe04be99a4809c52235c555097c484170
daqi <daqi@xiaomi.com>
2017-07-09 11:28:00 +0800

    dynamic register method

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -27,3 +27,27 @@ Java_org_peace_allinone_ui_MainActivity_stringFromJNI(
     return (jstring) env->CallStaticObjectMethod(cls, mid);
 }
 
+JNIEnv* env;
+jstring native_hello() {
+    return env->NewStringUTF("native hello");
+}
+
+JNINativeMethod methods[] = {
+        {"native_hello", "()Ljava/lang/String;", (void*)native_hello}
+};
+
+void registerNativeMethods() {
+    jclass cls = env->FindClass("org/peace/allinone/ui/MainActivity");
+    LOGE("RegisterNatives begin");
+    env->RegisterNatives(cls, methods, 1);
+    LOGE("RegisterNatives done");
+}
+
+jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    LOGE("JNI_OnLoad");
+    vm->GetEnv((void **) &env, JNI_VERSION_1_4);
+    LOGE("get env done");
+    registerNativeMethods();
+    return JNI_VERSION_1_4;
+}
+
