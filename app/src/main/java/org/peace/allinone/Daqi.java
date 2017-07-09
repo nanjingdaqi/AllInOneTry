38240b46c95e61a1c7981d18ffebd6ebabfabf14
daqi <daqi@xiaomi.com>
2017-07-09 15:07:37 +0800

    jni alloc java object

---
+++ app/src/main/java/org/peace/allinone/Daqi.java
@@ -0,0 +1,15 @@
+package org.peace.allinone;
+
+import android.util.Log;
+
+/**
+ * Created by daqi on 17-7-9.
+ */
+
+public class Daqi {
+    public String msg = "Daqi";
+
+    public Daqi() {
+        Log.e("peace", "daqi init");
+    }
+}
