fa138cfd85249a3b0ca3db13e2e36a8eb1ec81be
daqi <daqi@xiaomi.com>
2017-07-21 09:52:14 +0800

    check volatile and sync smali

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -3,11 +3,14 @@ package org.peace.allinone.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
+    
+    public volatile boolean bl;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -18,5 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
 
+  }
+  
+  synchronized public void foo() {
+      synchronized (this) {
+          Toast.makeText(this, "foo", Toast.LENGTH_SHORT).show();
+      }
   }
 }
