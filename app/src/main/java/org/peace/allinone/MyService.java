2fe4fcfe40f921abd9a4c744fd6e6884c568c816
daqi <daqi@xiaomi.com>
2017-07-22 14:54:13 +0800

    check client uid

--- app/src/main/java/org/peace/allinone/MyService.java
+++ app/src/main/java/org/peace/allinone/MyService.java
@@ -2,17 +2,30 @@ package org.peace.allinone;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 public class MyService extends Service {
 
+    Handler handler = new Handler(Looper.getMainLooper());
+
   Test test = new Test.Stub() {
 
     @Override public void foo(String arg) throws RemoteException {
-      Toast.makeText(MyService.this, "server test get called", Toast.LENGTH_LONG).show();
+        long uid = Binder.getCallingUid();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MyService.this, "server test get called by uid: " + uid, Toast.LENGTH_LONG).show();
+            }
+        });
     }
   };
 
