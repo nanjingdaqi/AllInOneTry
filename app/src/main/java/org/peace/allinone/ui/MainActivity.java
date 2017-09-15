7162d1fce83ba4a6af70faa2fec03d1535d8299c
daqi <daqi@xiaomi.com>
2017-09-15 17:15:14 +0800

    half done

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,5 +1,10 @@
 package org.peace.allinone.ui;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,14 +14,31 @@ import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    ButterKnife.bind(this);
-  }
+        ButterKnife.bind(this);
+    }
 
-  @OnClick(R.id.start_btn) public void onClick(View v) {
+    @OnClick(R.id.start_btn)
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.android.settings", "TestSettings"));
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
 
-  }
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("daqi_title");
+        builder.setContentText("daqi-content");
+        builder.setContentIntent(pi);
+        builder.setSmallIcon(android.R.drawable.ic_popup_sync);
+        builder.setWhen(0);
+        builder.setOngoing(true);
+        builder.setAutoCancel(false);
+
+        Notification notification = builder.build();
+
+        ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(100, notification);
+    }
 }
