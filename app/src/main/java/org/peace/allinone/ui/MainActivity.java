92a3a78df3ef937f706cb65e98e6f14019132916
daqi <daqi@xiaomi.com>
2017-09-15 16:56:35 +0800

    check BootClassLoader

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,8 +1,11 @@
 package org.peace.allinone.ui;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.app.AppLaunchChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -11,9 +14,12 @@ import butterknife.OnClick;
 import me.ele.base.utils.AppLogger;
 import org.peace.allinone.R;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        AppLogger.e(System.getProperty("java.library.path"));
+
+        AppLogger.e(System.getProperty("java.class.path"));
     }
 
     @OnClick(R.id.start_btn)
@@ -35,12 +45,28 @@ public class MainActivity extends AppCompatActivity {
             AppLogger.e("get id: " + (System.currentTimeMillis() - start));
             Toast.makeText(this, phoneRes.getString(resId), Toast.LENGTH_LONG).show();
 
-            Class PhoneLog_cls = Class.forName("com.android.services.telephony.Log", true, phoneCtx.getClassLoader());
-            Method pii_mtd = PhoneLog_cls.getDeclaredMethod("pii", Object.class);
-            String result = (String) pii_mtd.invoke(PhoneLog_cls, this);
-            AppLogger.e("result: " + result);
+//            Class PhoneLog_cls = Class.forName("com.android.services.telephony.Log", true, phoneCtx.getClassLoader());
+//            Method pii_mtd = PhoneLog_cls.getDeclaredMethod("pii", Object.class);
+//            String result = (String) pii_mtd.invoke(PhoneLog_cls, this);
+//            AppLogger.e("result: " + result);
+
+            Class SimActivationActivity_cls = Class.forName("com.android.services.telephony.activation.SimActivationActivity", true, phoneCtx.getClassLoader());
+            Constructor SimActivationActivity_ctr = SimActivationActivity_cls.getDeclaredConstructor();
+            SimActivationActivity_ctr.setAccessible(true);
+            activity = (Activity) SimActivationActivity_cls.newInstance();
+            AppLogger.e("activity: " + activity);
+
+            ClassLoader parentCl = getClassLoader().getParent();
+            AppLogger.e("parent cl: " + parentCl);
+            Class ZygoteInit_cls = parentCl.loadClass("com.android.internal.os.ZygoteInit");
+            AppLogger.e("zygote_init: " + ZygoteInit_cls);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
     }
 }
