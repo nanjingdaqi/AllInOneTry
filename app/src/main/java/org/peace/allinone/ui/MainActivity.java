ade0720c737cd0119d0ed8c9d74175a8c25665e6
daqi <daqi@xiaomi.com>
2017-11-02 15:33:04 +0800

    test appt2 link

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,22 +1,14 @@
 package org.peace.allinone.ui;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import org.peace.allinone.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    ButterKnife.bind(this);
-  }
-
-  @OnClick(R.id.start_btn) public void onClick(View v) {
-
   }
 }
