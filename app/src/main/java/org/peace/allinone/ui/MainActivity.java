5a4f8154714aaf71595a2cff539094bce0d2f4f4
daqi <daqi@xiaomi.com>
2017-11-25 15:20:55 +0800

    test style string handling in compile and runtime

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -3,6 +3,7 @@ package org.peace.allinone.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import org.peace.allinone.R;
 
 public class MainActivity extends Activity {
@@ -10,5 +11,14 @@ public class MainActivity extends Activity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    TextView tv = findViewById(R.id.tv1);
+    tv.setText(getText(R.string.foo1));
+
+    TextView tv2 = findViewById(R.id.tv2);
+    tv2.setText(getText(R.string.foo2));
+
+    TextView tv3 = findViewById(R.id.tv3);
+    tv3.setText(getText(R.string.foo3));
   }
 }
