c0a0ec99d45d869ed5dcada52e8336fbc2911b39
daqi <daqi@xiaomi.com>
2017-12-25 10:11:10 +0800

    test webview#loadurl 耗时

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -3,11 +3,17 @@ package org.peace.allinone.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import me.ele.base.utils.AppLogger;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.wv)
+  WebView wv;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -17,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
-
+    long tm = System.currentTimeMillis();
+      wv.loadUrl("http://www.google.com");
+    AppLogger.e("tm: " + (System.currentTimeMillis() - tm)/ 1000);
   }
 }
