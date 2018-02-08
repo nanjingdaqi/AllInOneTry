472af0b056de8cfae6063b6705c5a4ee4cbafcd3
daqi <daqi@xiaomi.com>
2018-02-08 09:24:03 +0800

    try open xiaoai

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -5,12 +5,17 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +30,15 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.start_btn)
     public void onClick(View v) {
         Intent intent = new Intent();
+        // intent.setPackage("com.miui.voiceassist");
+        intent.setAction("miui.intent.action.APP_SETTINGS");
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(intent, 0);
+        for (ResolveInfo info : list) {
+            Log.d("daqi", "info: " + info);
+        }
 //        intent.setComponent(new ComponentName("com.android.settings", "TestSettings"));
-        intent.setComponent(new ComponentName("com.android.settings", "com.android.settings.TestSettings"));
+//        intent.setComponent(new ComponentName("com.android.settings", "com.android.settings.TestSettings"));
 //        intent = new Intent(this, MainActivity.class);
 //        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
 //
