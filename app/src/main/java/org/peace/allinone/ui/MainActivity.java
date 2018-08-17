be2a4aeac2849068638bb2ea4db017cda7e637ec
daqi <daqi@xiaomi.com>
2018-08-17 19:43:29 +0800

    test xiaoai_support

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,22 +1,51 @@
 package org.peace.allinone.ui;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.ServiceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.android.internal.view.IInputMethodManager;
 import org.peace.allinone.R;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    IInputMethodManager imms;
 
-    ButterKnife.bind(this);
-  }
+    Method commitTextMethod;
 
-  @OnClick(R.id.start_btn) public void onClick(View v) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-  }
+        ButterKnife.bind(this);
+
+        imms = IInputMethodManager.Stub.asInterface(ServiceManager.getService(Context.INPUT_METHOD_SERVICE));
+        try {
+            commitTextMethod = IInputMethodManager.class.getMethod("commitText", String.class, int.class);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @OnClick(R.id.et)
+    public void onClick(View v) {
+        v.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    commitTextMethod.invoke(imms, "Hello Daqi", 0);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, 3000);
+    }
+
 }
