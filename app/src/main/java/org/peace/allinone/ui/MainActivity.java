56d9938f04f2ec9dc1da076db232698ad091dcca
daqi <daqi@xiaomi.com>
2018-09-20 17:33:39 +0800

    install app with dm

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,9 +1,16 @@
 package org.peace.allinone.ui;
 
+import android.annotation.TargetApi;
 import android.app.ActivityThread;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.IPackageManager;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageInstaller;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageParser;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +19,11 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.Locale;
 
@@ -40,14 +52,52 @@ public class MainActivity extends AppCompatActivity {
 //        }
     }
 
+    File apk = new File("/sdcard/wechat.apk");
+    File dm = new File("/sdcard/wechat.dm");
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @OnClick(R.id.start_btn)
     public void onClick(View v) {
-        IPackageManager pm = ActivityThread.getPackageManager();
         try {
-            pm.installPackageAsUser("/data/sdcard/app-debug.apk", null, 0, "", 0);
-            pm.installPackageAsUser("/data/sdcard/复现Demo.apk", null, 0, "", 0);
-        } catch (RemoteException e) {
+            PackageManager pm = getPackageManager();
+            PackageInstaller.SessionParams params = new PackageInstaller.SessionParams(
+                    PackageInstaller.SessionParams.MODE_FULL_INSTALL);
+            params.setInstallLocation(PackageInfo.INSTALL_LOCATION_INTERNAL_ONLY);
+            PackageInstaller pi = pm.getPackageInstaller();
+            int sessionId  = pi.createSession(params);
+            PackageInstaller.Session session = pi.openSession(sessionId);
+
+            OutputStream os = session.openWrite("wechat.apk", 0, apk.length());
+            InputStream is = new FileInputStream(apk);
+            byte[] buffer = new byte[4086];
+            int len = 0;
+            while ((len = is.read(buffer)) > 0) {
+                os.write(buffer, 0, len);
+            }
+            session.fsync(os);
+            is.close();
+            os.close();
+
+            os = session.openWrite("wechat.dm", 0, dm.length());
+            is = new FileInputStream(dm);
+            while ((len = is.read(buffer)) > 0) {
+                os.write(buffer, 0, len);
+            }
+            session.fsync(os);
+            is.close();
+            os.close();
+
+            Intent broadcastIntent = new Intent("org.peace");
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                    this /*context*/,
+                    sessionId,
+                    broadcastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+
+            session.commit(pendingIntent.getIntentSender());
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 }
