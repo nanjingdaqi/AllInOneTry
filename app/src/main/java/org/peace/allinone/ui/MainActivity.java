fe1618397dca299fdd6e004cdf5243584d503746
daqi <daqi@xiaomi.com>
2018-09-21 10:49:05 +0800

    复现输入法窗口无法关闭

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package org.peace.allinone.ui;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import butterknife.ButterKnife;
@@ -9,14 +11,33 @@ import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    ButterKnife.bind(this);
-  }
+        ButterKnife.bind(this);
+    }
 
-  @OnClick(R.id.start_btn) public void onClick(View v) {
+    @OnClick(R.id.start_btn)
+    public void onClick(View v) {
 
-  }
+        final AlertDialog dlg = new AlertDialog.Builder(this)
+                .setView(R.layout.dlg)
+                .show();
+
+        dlg.findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dlg.dismiss();
+            }
+        });
+
+        dlg.findViewById(R.id.show).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextInputEditText) dlg.findViewById(R.id.et)).setError("Haha");
+            }
+        });
+    }
 }
