83c8ae78c8cd0bdee256192e5362e93599efa984
daqi <daqi@xiaomi.com>
2018-09-29 09:30:16 +0800

    FLAG_DIM_BEHIND

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,9 +1,15 @@
 package org.peace.allinone.ui;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.afollestad.materialdialogs.GravityEnum;
@@ -11,28 +17,32 @@ import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.LinkedList;
 import java.util.List;
+
 import me.ele.base.utils.DimenUtil;
 import org.peace.allinone.R;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    ButterKnife.bind(this);
-  }
+        ButterKnife.bind(this);
+    }
 
-  @OnClick(R.id.start_btn) public void onClick(View v) {
-    BottomSheetDialog dlg = new BottomSheetDialog(this);
-    dlg.setContentView(R.layout.dlg);
-    dlg.show();
-//    getWindow().getDecorView().postDelayed(new Runnable() {
-//      @Override
-//      public void run() {
-//          MainActivity.this.finish();
-//      }
-//    }, 5000);
-  }
+    @OnClick(R.id.start_btn)
+    public void onClick(View v) {
+        Dialog dlg = new Dialog(this);
+        dlg.setContentView(R.layout.dlg);
+        Window w = dlg.getWindow();
+        WindowManager.LayoutParams lp = w.getAttributes();
+        lp.flags = lp.flags | WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+//      lp.flags = lp.flags & ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        lp.dimAmount = .8f;
+
+        w.setAttributes(lp);
+        dlg.show();
+    }
 }
 
