2036d34eef70ae1d1f6d793c227ec330bb983a84
daqi <daqi@xiaomi.com>
2018-10-24 16:52:59 +0800

    test intent-filter priority

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -29,31 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.start_btn)
     public void onClick(View v) {
-        Intent intent = new Intent();
-        // intent.setPackage("com.miui.voiceassist");
-        intent.setAction("miui.intent.action.APP_SETTINGS");
-        PackageManager pm = getPackageManager();
-        List<ResolveInfo> list = pm.queryIntentActivities(intent, 0);
-        for (ResolveInfo info : list) {
-            Log.d("daqi", "info: " + info);
-        }
-//        intent.setComponent(new ComponentName("com.android.settings", "TestSettings"));
-//        intent.setComponent(new ComponentName("com.android.settings", "com.android.settings.TestSettings"));
-//        intent = new Intent(this, MainActivity.class);
-//        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
-//
-//        Notification.Builder builder = new Notification.Builder(this);
-//        builder.setContentTitle("daqi_title");
-//        builder.setContentText("daqi-content");
-//        builder.setContentIntent(pi);
-//        builder.setSmallIcon(android.R.drawable.ic_popup_sync);
-//        builder.setWhen(0);
-//        builder.setOngoing(true);
-//        builder.setAutoCancel(false);
-//
-//        Notification notification = builder.build();
-//
-//        ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(100, notification);
+        Intent intent = new Intent("android.intent.action.VIEW_LICENSE");
 
         startActivity(intent);
     }
---
+++ app/src/main/java/org/peace/allinone/ui/SubActivity.java
@@ -0,0 +1,6 @@
+package org.peace.allinone.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+}
