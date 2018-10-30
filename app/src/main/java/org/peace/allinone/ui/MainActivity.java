18f932e9f0a51e294944556f20d5625ea9dde0e7
daqi <daqi@xiaomi.com>
2018-10-30 11:26:21 +0800

    check state_hidden back to pre page

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,10 +1,13 @@
 package org.peace.allinone.ui;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
@@ -14,7 +17,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Window w = getWindow();
+        WindowManager.LayoutParams lp = w.getAttributes();
+        lp.softInputMode |= WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN;
+        w.setAttributes(lp);
 
         ButterKnife.bind(this);
     }
@@ -22,22 +31,6 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.start_btn)
     public void onClick(View v) {
 
-        final AlertDialog dlg = new AlertDialog.Builder(this)
-                .setView(R.layout.dlg)
-                .show();
-
-        dlg.findViewById(R.id.close).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dlg.dismiss();
-            }
-        });
-
-        dlg.findViewById(R.id.show).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((TextInputEditText) dlg.findViewById(R.id.et)).setError("Haha");
-            }
-        });
+        startActivity(new Intent(this, SubActivity.class));
     }
 }
