f2971c6d57156aff0e7c9052b38d6234cde2fead
daqi <daqi@xiaomi.com>
2018-11-28 09:34:56 +0800

    check date format

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -2,10 +2,15 @@ package org.peace.allinone.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import org.peace.allinone.R;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
   @OnClick(R.id.start_btn) public void onClick(View v) {
 
+      int pid = android.os.Process.myPid();
+      int tid = (int) Thread.currentThread().getId();
+      String time = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM, Locale.CHINA).format(new Date());
+      String content = String.format(Locale.US, "%s  %d  %d  daqi: %s\n", time, pid, tid, "text_log");
+
+      Log.d("daqi", content);
   }
 }
