2a9faba2b006510c9b3b56da5886cb2b7be512b4
daqi <daqi@xiaomi.com>
2018-12-30 17:22:00 +0800

    1. ObserveOn内部存在一个drain机制，其内部worker负责调用消费者进行消费，如果上游的生产速度比自己的消费动作快，则不会调用worker#schedule动作，通常也就不会拉新线程，现象就是生产者的数据都在一个线程下处理了 2. 反之，消费速度比生产速度快，则每次都会新起一次schedul动作 3. 具体见ObsavableObserveOn#schedule()的实现，内部会根据自身(AtomicInteger)的大小做处理

--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package org.peace.allinone.ui;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -9,29 +8,37 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
-import io.reactivex.Scheduler;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import org.peace.allinone.R;
-import android.os.Process;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+
+import static android.os.Process.myTid;
 
 public class MainActivity extends AppCompatActivity {
 
     static void L(Object msg) {
-        Log.d("daqi", "" + msg + " tid: " + Process.myTid() + ", thread_name: " + Thread.currentThread().getName());
+        Log.d("daqi", "" + msg + " tid: " + myTid() + ", thread_name: " + Thread.currentThread().getName());
     }
 
 
     Executor e1 = command -> {
-        Thread t1 = new Thread(command, "T1");
+        Log.d("daqi", "exec cmd at e1 tid: " + myTid());
+        Thread t1 = new Thread(() -> {
+            Log.d("daqi", "T1 runs at tid: " + myTid());
+            command.run();
+        }, "T1");
         t1.start();
     };
 
     Executor e2 = command -> {
-        Thread t2 = new Thread(command, "T2");
+        Log.d("daqi", "exec cmd at e2 tid: " + myTid(), new Exception());
+        Thread t2 = new Thread(() -> {
+            Log.d("daqi", "T2 runs at tid: " + myTid());
+            command.run();
+        }, "T2");
         t2.start();
     };
 
@@ -40,27 +47,21 @@ public class MainActivity extends AppCompatActivity {
         t3.start();
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ButterKnife.bind(this);
-
+    public void testThread() {
         Observable.fromCallable(() -> {
-            Log.d("daqi", "Observable: " + Process.myTid() + ", thread name: " + Thread.currentThread().getName());
+            Log.d("daqi", "Observable: " + myTid() + ", thread name: " + Thread.currentThread().getName());
             return "Hello";
         })
                 .subscribeOn(Schedulers.from(e1))
                 .observeOn(Schedulers.from(e2))
                 .map(v -> {
-                    Log.d("daqi", "map1: " + Process.myTid() + ", thread name: " + Thread.currentThread().getName());
+                    Log.d("daqi", "map1: " + myTid() + ", thread name: " + Thread.currentThread().getName());
                     return "Map result";
                 })
                 .subscribeOn(Schedulers.from(e1))
                 .observeOn(Schedulers.from(e3))
                 .map(v -> {
-                    Log.d("daqi", "map2: " + Process.myTid() + ", thread name: " + Thread.currentThread().getName());
+                    Log.d("daqi", "map2: " + myTid() + ", thread name: " + Thread.currentThread().getName());
                     return "Map2 result";
                 })
                 .subscribe(new Observer<String>() {
@@ -84,6 +85,55 @@ public class MainActivity extends AppCompatActivity {
                         L("complete");
                     }
                 });
+    }
+
+    public void asyncJob(int snds) {
+        try {
+            Thread.sleep(snds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cpt(int snd) {
+        long tm = System.currentTimeMillis();
+        while (System.currentTimeMillis() - tm <= snd) {
+
+        }
+    }
+
+    public void testFlatMap() {
+        Observable<Integer> source = Observable.fromArray(1, 2, 3, 4, 5);
+        Function<Integer, Observable<Integer>> fm = val -> {
+            Log.d("daqi", "flat_map for: " + val + ", thread_name: " + Thread.currentThread().getName());
+//            asyncJob(200 - val * 10);
+//            asyncJob(val * 1);
+//            asyncJob(1);observe
+//            cpt(100);
+            return Observable.fromArray(val, val * val);
+        };
+
+        source
+                .observeOn(Schedulers.from(e1))
+                .flatMap(fm)
+                .observeOn(Schedulers.from(e2))
+//                .concatMap(fm)
+                .subscribe((integer) -> {
+                    cpt(100);
+                    Log.d("daqi", "" + integer + "  " + myTid() + ", tname: " + Thread.currentThread().getName());
+                });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+//        testThread();
+
+        testFlatMap();
     }
 
     @OnClick(R.id.start_btn)
